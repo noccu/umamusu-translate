38c17803d33d0760c8f1d17f20c988e17b5d14d8
noccu <37862870+noccu@users.noreply.github.com>
2021-11-09 06:56:54 +0100

    Add script to restore files to originals
    Based on rockisch' tools.

---
+++ src/restore-files.py
@@ -0,0 +1,52 @@
+import requests
+import common
+from common import GAME_ASSET_ROOT
+from os.path import join, realpath, isfile
+import shutil
+
+# Globals & Parameter parsing
+HOSTNAME = 'https://prd-storage-umamusume.akamaized.net/dl/resources'
+ASSETS_ENDPOINT = HOSTNAME + '/Windows/assetbundles/{0:.2}/{0}'
+LOCAL_DUMP_DIR = realpath("dump") # A folder where original files were copied to before. (and not modified)
+
+args = common.Args().parse()
+if args.getArg("-h"):
+    common.usage("[-g <group>] [-id <id>] [-src <file>]",
+                 "-src overwrites other options")
+
+TARGET_GROUP = args.getArg("-g", False)
+TARGET_ID = args.getArg("-id", False)
+TARGET_FILE = args.getArg("-src", False)
+FORCE_DL = args.getArg("-F", False)
+
+
+def download(file):
+    url = ASSETS_ENDPOINT.format(file)
+    print(f"Downloading {file} from {url}")
+    return requests.get(url)
+
+def save(fileName):
+    dstPath = join(GAME_ASSET_ROOT, fileName[:2], fileName)
+    localFile = join(LOCAL_DUMP_DIR, fileName[:2], fileName)
+
+    print(f"Saving file to {dstPath}")
+    if not FORCE_DL and isfile(localFile):
+        print (f"Copying file from {localFile}")
+        shutil.copyfile(localFile, dstPath)
+    else:
+        data = download(fileName)
+        if data.status_code == 200:
+            with open(dstPath, "wb") as f:
+                f.write(data.content)
+        else:
+            print(f"Error downloading file {fileName}")
+
+#* Main
+if TARGET_FILE:
+    save(TARGET_FILE)
+else:
+    files = common.searchFiles(TARGET_GROUP, TARGET_ID)
+    for file in files:
+        parsed = common.readJson(file)
+        for bundleName in parsed.keys():
+            save(bundleName)

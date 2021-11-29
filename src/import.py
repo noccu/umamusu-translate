noccu/umamusu-translate

commit acb765461c048676a5a3b94ccc55035064ecd46f

Support race stories
It's a special type used for some main story eps, from the looks of it.

src/import.py
@@ -26,55 +26,67 @@ def get_meta(filePath: str) -> tuple[UnityPy.environment.Environment, UnityPy.en
 def swapAssetData(tlFile: TranslationFile):
     bundle = tlFile.getBundle()
     textList = tlFile.getTextBlocks()
+    assetType = tlFile.getType()
     assetPath = os.path.join(GAME_ASSET_ROOT, bundle[0:2], bundle)
 
     if not os.path.exists(assetPath):
         return f"AssetBundle {bundle} does not exist in your game data, skipping..."
 
     try:
-        env, metadata = get_meta(assetPath)
+        env, mainFile = get_meta(assetPath)
     except Exception as e:
         return f"UnityPy Error: {repr(e)}, skipping {bundle}..."
 
-    assetList = metadata.assets_file.files
-    assetsSkipped = 0
+    assetList = mainFile.assets_file.files
+    textBlocksSkipped = 0
+    lockAsset = False
 
-    for textData in textList:
+    for textIdx, textData in enumerate(textList):
         if not textData['enText']:
-            assetsSkipped += 1
+            textBlocksSkipped += 1
             continue
 
-        try:
-            asset = assetList[textData['pathId']]
-        except KeyError:
-            print(f"Skipping block {textData['blockIdx']} in {bundle}: Can't find pathId in original asset")
-            continue
+        if assetType == "race":
+            if not lockAsset:
+                asset = mainFile
+                assetData = asset.read_typetree()
+                lockAsset = True
+        else:
+            try:
+                asset = assetList[textData['pathId']]
+                assetData = asset.read_typetree()
+            except KeyError:
+                print(f"Skipping block {textData['blockIdx']} in {bundle}: Can't find pathId in original asset")
+                continue
 
-        assetData = asset.read_typetree()
-        assetData['Text'] = textData['enText']
-        assetData['Name'] = textData['enName'] or assetData['Name']
+        if assetType == "race":
+            assetData['textData'][textIdx]['text'] = textData['enText']
+        else:
+            assetData['Text'] = textData['enText']
+            assetData['Name'] = textData['enName'] or assetData['Name']
 
-        if 'choices' in textData:
-            jpChoices, enChoices = assetData['ChoiceDataList'], textData['choices']
-            if len(jpChoices) != len(enChoices):
-                print("Choice lenghts do not match, skipping...")
-            else:
-                for idx, choice in enumerate(textData['choices']):
-                    # ? Not sure if guaranteed same order. Maybe do a search on jpText instead?
-                    if choice['enText']:
-                        jpChoices[idx]['Text'] = choice['enText']
+            if 'choices' in textData:
+                jpChoices, enChoices = assetData['ChoiceDataList'], textData['choices']
+                if len(jpChoices) != len(enChoices):
+                    print("Choice lenghts do not match, skipping...")
+                else:
+                    for idx, choice in enumerate(textData['choices']):
+                        # ? Not sure if guaranteed same order. Maybe do a search on jpText instead?
+                        if choice['enText']:
+                            jpChoices[idx]['Text'] = choice['enText']
 
-        if 'coloredText' in textData:
-            jpColored, enColored = assetData['ColorTextInfoList'], textData['coloredText']
-            if len(jpColored) != len(enColored):
-                print("Colored text lenghts do not match, skipping...")
-            else:
-                for idx, text in enumerate(textData['coloredText']):
-                    if text['enText']:
-                        jpColored[idx]['Text'] = text['enText']
+            if 'coloredText' in textData:
+                jpColored, enColored = assetData['ColorTextInfoList'], textData['coloredText']
+                if len(jpColored) != len(enColored):
+                    print("Colored text lenghts do not match, skipping...")
+                else:
+                    for idx, text in enumerate(textData['coloredText']):
+                        if text['enText']:
+                            jpColored[idx]['Text'] = text['enText']
+            asset.save_typetree(assetData)
+    if assetType == "race": asset.save_typetree(assetData)
 
-        asset.save_typetree(assetData)
-    if assetsSkipped == len(textList):
+    if textBlocksSkipped == len(textList):
         env = None
 
     return env

src/common.py
@@ -88,6 +88,12 @@ class TranslationFile:
         else:
             return list(self.data.keys())[0]
 
+    def getType(self):
+        if self.version > 2:
+            return self.data['type']
+        else:
+            return "story/home"
+
     def save(self):
         writeJsonFile(self.file, self.data)
 

src/extract.py
@@ -13,7 +13,7 @@ if args.getArg("-h"):
                  "Any order. Defaults to extracting all text, skip existing")
 
 EXTRACT_TYPE = args.getArg("-t", "story").lower()
-if not EXTRACT_TYPE in ["story", "home"]: 
+if not EXTRACT_TYPE in ["story", "home", "race"]: 
     print(f"Invalid type {EXTRACT_TYPE}")
     raise SystemExit
 EXTRACT_GROUP = args.getArg("-g", "__")
@@ -29,6 +29,8 @@ def queryDB():
         pattern = f"{EXTRACT_TYPE}/data/{EXTRACT_GROUP}/{EXTRACT_ID}/{EXTRACT_TYPE}timeline%"
     elif EXTRACT_TYPE == "home":
         pattern = f"{EXTRACT_TYPE}/data/00000/{EXTRACT_GROUP}/{EXTRACT_TYPE}timeline_00000_{EXTRACT_GROUP}_{EXTRACT_ID}%"
+    elif EXTRACT_TYPE == "race":
+        pattern = f"race/storyrace/text/storyrace_{EXTRACT_GROUP}{EXTRACT_ID}%"
     db = sqlite3.connect(GAME_META_FILE)
     cur = db.execute(
         f"select h, n from a where n like '{pattern}' limit {EXTRACT_LIMIT};")
@@ -44,27 +46,46 @@ def extractAsset(path, storyId):
     if index.serialized_type.nodes:
         tree = index.read_typetree()
         export = {
-            'version': 2,
+            'version': 3,
             'bundle': env.file.name,
-            'storyId': tree['StoryId'],
-            'title' : tree['Title'],
+            'type': EXTRACT_TYPE,
+            'storyId': "",
+            'title' : "",
             'text': list()
         }
-        for block in tree['BlockList']:
-            for clip in block['TextTrack']['ClipList']:
-                pathId = clip['m_PathID']
-                textData = extractText(index.assets_file.files[pathId])
-                if not textData:
-                    continue
-                textData['pathId'] = pathId  # important for re-importing
-                textData['blockIdx'] = block['BlockIndex'] # to help translators look for specific routes
-                transferExisting(storyId, textData)
+
+        if EXTRACT_TYPE == "race":
+            export['storyId'] = tree['m_Name'][-9:]
+
+            for block in tree['textData']:
+                textData = extractText("race", block)
                 export['text'].append(textData)
+        else:
+            export['storyId'] = tree['StoryId']
+            export['title'] = tree['Title']
+
+            for block in tree['BlockList']:
+                for clip in block['TextTrack']['ClipList']:
+                    pathId = clip['m_PathID']
+                    textData = extractText(EXTRACT_TYPE, index.assets_file.files[pathId])
+                    if not textData:
+                        continue
+                    textData['pathId'] = pathId  # important for re-importing
+                    textData['blockIdx'] = block['BlockIndex'] # to help translators look for specific routes
+                    transferExisting(storyId, textData)
+                    export['text'].append(textData)
         return export
 
 
-def extractText(obj):
-    if obj.serialized_type.nodes:
+def extractText(assetType, obj):
+    if assetType == "race":
+        # obj is already read
+        o = {
+            'jpText': obj['text'],
+            'enText': "",
+            'blockIdx': obj['key']
+        }
+    elif obj.serialized_type.nodes:
         tree = obj.read_typetree()
         o = {
             'jpName': tree['Name'],
@@ -92,7 +113,7 @@ def extractText(obj):
                     'enText': ""
                 })
 
-        return o if o['jpText'] else None
+    return o if o['jpText'] else None
 
 def transferExisting(storyId, textData):
     if OVERWRITE_DST: return
@@ -128,14 +149,15 @@ def exportData(data, filepath: str):
 
 
 def parseStoryId(path) -> tuple:
-    if EXTRACT_TYPE == "story":
-        storyId = path[-9:]
-        return  storyId[:2], storyId[2:6], storyId[6:9]
-    elif EXTRACT_TYPE == "home":
+    if EXTRACT_TYPE == "home":
         # storyId = path[-16:]
         # return storyId[:5], storyId[6:8], storyId[9:13], storyId[13:]
         storyId = path[-10:]
         return storyId[:2], storyId[3:7], storyId[7:]
+    else:
+        # story and storyrace
+        storyId = path[-9:]
+        return  storyId[:2], storyId[2:6], storyId[6:9]
         
 
 
@@ -145,7 +167,7 @@ def exportAsset(bundle: str, path: str):
 
     # check existing files first
     if not OVERWRITE_DST:
-        search = exportDir.glob(f"{idx} *.json")
+        search = exportDir.glob(f"{idx}*.json")
         for file in search:
             if file.exists():
                 print(f"Skipping existing: {file.name}")
@@ -156,7 +178,7 @@ def exportAsset(bundle: str, path: str):
 
     #remove stray control chars
     title = "".join(c for c in data['title'] if ord(c) > 31)
-    idxString = f"{idx} ({title})"
+    idxString = f"{idx} ({title})" if title else idx
 
     exportPath = f"{os.path.join(exportDir, idxString)}.json"
     exportData(data, exportPath)
@@ -165,7 +187,8 @@ def exportAsset(bundle: str, path: str):
 def main():
     print(f"Extracting group {EXTRACT_GROUP}, id {EXTRACT_ID} (limit {EXTRACT_LIMIT or 'ALL'}, overwrite: {OVERWRITE_DST})\nfrom {GAME_ASSET_ROOT} to {EXPORT_DIR}")
     q = queryDB()
+    print(f"Found {len(q)} files.")
     for bundle, path in q:
         exportAsset(bundle, path)
-
+    print("Processing finished sucessfully.")
 main()

src/machinetl.py
@@ -60,8 +60,8 @@ class Translator:
             for entry in self._entryGenerator(file):
                 # Skip already translated text
                 if not entry['enText']:
-                    text = textprocess.process(entry['jpText'], {"noNewlines": True})
-                    entry['enText'] = textprocess.process(await self.requestTl(text), {"lineLen": False, "replace": True}) # defer to default
+                    text = textprocess.process(file, entry['jpText'], {"noNewlines": True})
+                    entry['enText'] = textprocess.process(file, await self.requestTl(text), {"lineLen": False, "replace": True}) # defer to default
             file.save()
         await self.client.close()
         STOP.set_result(True)

src/textprocess.py
@@ -1,10 +1,11 @@
 import common
+from common import TranslationFile
 import re
 from math import ceil
 
 args = common.Args().parse()
 if args.getArg("-h"):
-    common.usage("[-g <group>] [-id <id>] [-src <json file>] [-ll <line length>]",
+    common.usage("[-t <story|home|race>] [-g <group>] [-id <id>] [-src <json file>] [-ll <line length>]",
                  "At least 1 arg is required.",
                  "-src overwrites other options.")
 
@@ -16,19 +17,19 @@ LINE_LENGTH = int(args.getArg("-ll", 42)) #Roughly 42 for most training story di
 
 if not TARGET_FILE and not TARGET_GROUP and not TARGET_ID: raise SystemExit("At least 1 arg is required.")
 
-def process(text: str, options: dict):
+def process(file: TranslationFile, text: str, options: dict):
     if "noNewlines" in options:
-        text = cleannewLines(text)
+        text = cleannewLines(file, text)
     if "lineLen" in options:
-        text = adjustLength(text, options["lineLen"] or LINE_LENGTH)
+        text = adjustLength(file, text, options["lineLen"] or LINE_LENGTH)
     if "replace" in options:
         text = replace(text)
     return text
 
-def cleannewLines(text: str):
-    return re.sub("\r?\n", " ", text)
+def cleannewLines(file: TranslationFile, text: str):
+    return re.sub(r"\\n" if file.getType() == "race" else "\r?\n", " ", text)
 
-def adjustLength(text: str, lineLen: int = 0, numLines: int = 0, targetLines: int = 0):
+def adjustLength(file: TranslationFile, text: str, lineLen: int = 0, numLines: int = 0, targetLines: int = 0):
     if len(text) < lineLen:
         print("Short text line, skipping: ", text)
         return text
@@ -42,13 +43,13 @@ def adjustLength(text: str, lineLen: int = 0, numLines: int = 0, targetLines: in
             return text
 
         #adjust if not
-        text = cleannewLines(text)
+        text = cleannewLines(file, text)
         # python regexes kinda suck
         lines = re.findall(f"(?:(?<= )|(?<=^)).{{1,{lineLen}}}(?= |$)|(?<= ).+$", text)
         nLines = len(lines)
         if nLines > 1 and len(lines[-1]) < min(lineLen, len(text)) / nLines:
             print("Last line is short, balancing on line number: ", lines)
-            return adjustLength(text, numLines = nLines, targetLines=nLines)
+            return adjustLength(file, text, numLines = nLines, targetLines=nLines)
 
     elif numLines > 0:
         lineLen = ceil(len(text) / numLines)
@@ -56,7 +57,7 @@ def adjustLength(text: str, lineLen: int = 0, numLines: int = 0, targetLines: in
 
     if targetLines > 0 and len(lines) > targetLines:
         print("Exceeded target lines at:", lines)
-    return "\n".join(lines)
+    return "\\n".join(lines) if file.getType() == "race" else "\n".join(lines)
 
 def replace(text: str):
     data = common.readJson("src/data/replacer.json")
@@ -75,7 +76,7 @@ def main():
         file = common.TranslationFile(file)
         for block in file.getTextBlocks():
             if not "enText" in block or len(block['enText']) == 0: continue
-            block['enText'] = process(block['enText'], {"lineLen": LINE_LENGTH, "replace": True})
+            block['enText'] = process(file, block['enText'], {"lineLen": LINE_LENGTH, "replace": True})
         file.save()
     print("Files processed.")
 

commit 33ba2907da1aff3a7a1be43fdf323d4905c39c1b

Fix small regression from acb765461c048676a5a3b9

src/import.py
@@ -54,10 +54,10 @@ def swapAssetData(tlFile: TranslationFile):
         else:
             try:
                 asset = assetList[textData['pathId']]
-                assetData = asset.read_typetree()
             except KeyError:
                 print(f"Skipping block {textData['blockIdx']} in {bundle}: Can't find pathId in original asset")
                 continue
+            assetData = asset.read_typetree()
 
         if assetType == "race":
             assetData['textData'][textIdx]['text'] = textData['enText']
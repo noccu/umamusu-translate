noccu/umamusu-translate

commit 269bd3fe1c6fe20c7c6c97175ffc083a26828d16

Optimize extract script with existing files
Skip them before loading assets. (when not forced)
Also minor replacer update.

src/extract.py
@@ -22,7 +22,7 @@ OVERWRITE_DST = args.getArg("-O", False)
 def queryDB():
     db = sqlite3.connect(GAME_META_FILE)
     cur = db.execute(
-        f"select h from a where n like 'story/data/{EXTRACT_GROUP}/{EXTRACT_ID}/storytimeline%' limit {EXTRACT_LIMIT};")
+        f"select h,n from a where n like 'story/data/{EXTRACT_GROUP}/{EXTRACT_ID}/storytimeline%' limit {EXTRACT_LIMIT};")
     results = cur.fetchall()
     db.close()
     return results
@@ -110,7 +110,7 @@ def transferExisting(storyId, textData):
 
 
 
-def exportData(data, filepath):
+def exportData(data, filepath: str):
     if OVERWRITE_DST == True or not os.path.exists(filepath):
         export = json.dumps(data, indent=4, ensure_ascii=False)
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
@@ -125,7 +125,16 @@ def parseStoryId(storyId):
         raise ValueError("Invalid Story ID format.")
 
 
-def exportAssets(bundle: str):
+def exportAssets(bundle: str, path: str):
+    # check existing files first
+    if not OVERWRITE_DST:
+        group, id, idx = parseStoryId(path[-9:])
+        search = Path(EXPORT_DIR).joinpath(group, id).glob(f"{idx} *.json")
+        for file in search:
+            if file.exists():
+                print(f"Skipping existing: {file.name}")
+                return
+
     importPath = os.path.join(GAME_ASSET_ROOT, bundle[0:2], bundle)
     data = extractAssets(importPath)
 
@@ -142,7 +151,7 @@ def exportAssets(bundle: str):
 def main():
     print(f"Extracting group {EXTRACT_GROUP}, id {EXTRACT_ID} (limit {EXTRACT_LIMIT or 'ALL'}, overwrite: {OVERWRITE_DST})\nfrom {GAME_ASSET_ROOT} to {EXPORT_DIR}")
     q = queryDB()
-    for bundle, in q:
-        exportAssets(bundle)
+    for bundle, path in q:
+        exportAssets(bundle, path)
 
 main()

commit 28348144b06b8380e8af3ee45a1e1d657ac982d8

Transfer existing text when overwriting extracts
Regression in 269bd3fe1c6fe20c7c6c97175ffc083a26828d16.
Not sure how it worked after this but I don't think as intended/at all.

That commit added early skip for existing files, breaking any transfers,
and the only time the transfer function ran was when given -O, in which
case it terminated immediately as it interpreted -O as "full reset".
For such "full reset" behaviour just delete the file and re-extract.

src/extract.py
@@ -86,10 +86,9 @@ def extractText(obj):
         return o if o['jpText'] else None
 
 def transferExisting(storyId, textData):
-    if OVERWRITE_DST: return
     group, id, idx = parseStoryId(storyId)
     existing = None
-    search = Path(EXPORT_DIR).joinpath(group, id).glob(f"{idx} *")
+    search = Path(EXPORT_DIR).joinpath(group, id).glob(f"{idx}*")
     for file in search:
         if file.is_file():
             existing = common.TranslationFile(file)
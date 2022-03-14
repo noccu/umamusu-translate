ad2d0cc60f2f2f4747ab823f2a5a7a44d6a31eed
noccu <37862870+noccu@users.noreply.github.com>
2022-03-14 18:38:29 +0100

    Add fairseq trained model translation option
    This is a first step to incorporating custom machine learning tl.
    Based on MingShiba's trained model for VNOCR.
    
    From my testing it does better at some things but is overall worse.
    A few tls in test files have been updated but they are hand picked.
    
    However, the possibility is there now for any model to be used.

---
+++ src/data/sugoi-model/sugoi-readme.txt
@@ -0,0 +1,20 @@
+This model is trained by MingShiba as part of Sugoi Translator/VN OCR.
+
+How to use it:
+Install fairseq >=10.0.2
+Currently there is an issue with the pip version on windows, so you need to clone the git repo and install it yourself:
+
+git clone https://github.com/pytorch/fairseq
+cd fairseq
+pip install --editable ./
+
+
+Then install sentencepiece:
+
+py -m pip install sentencepiece
+
+Download the model files from https://drive.google.com/file/d/1KgAsQzI-A0D8vmNIekwIRKlflGw9xz3i/view
+And extract the archive in the sugoi-model folder
+You should end up with src/data/sugoi-model/japaneseModel etc.
+
+Then run the usual machinetl.py with the extra arg: -model sugoi
--- src/machinetl.py
+++ src/machinetl.py
@@ -4,6 +4,8 @@ from websockets import server
 import json
 import common
 import textprocess
+from importlib import import_module
+from pathlib import Path
 
 # Globals & Parameter parsing
 args = common.Args().parse()
@@ -15,8 +17,11 @@ TARGET_GROUP = args.getArg("-g", False)
 TARGET_ID = args.getArg("-id", False)
 TARGET_IDX = args.getArg("-idx", False)
 TARGET_FILE = args.getArg("-src", False)
+TARGET_MODEL = args.getArg("-model", "deepl")
 LINE_LENGTH = int(args.getArg("-ll", False))
 OVERWRITE_TEXT = args.getArg("-O", False)
+
+SUGOI_ROOT = "src/data/sugoi-model"
 
 async def handler(client: server.WebSocketServerProtocol, path):
     print("New client connected")
@@ -38,11 +43,33 @@ async def startServer():
         print("Server started, awaiting connection to deepl script. See README for info.")
         await STOP  # run until stopped
 class Translator:
-    def __init__(self, client: server.WebSocketServerProtocol):
+    def __init__(self, client: server.WebSocketServerProtocol = None):
         if TARGET_FILE: self.files = [TARGET_FILE]
         else: self.files = common.searchFiles(TARGET_TYPE, TARGET_GROUP, TARGET_ID, TARGET_IDX)
-        self.client = client
-        self.loop = asyncio.get_running_loop()
+        if USING_SERVER:
+            self.client = client
+            
+        if TARGET_MODEL == "deepl":
+            self.loop = asyncio.get_running_loop()
+        elif TARGET_MODEL == "sugoi":
+            try:
+                self.model = getattr(import_module("fairseq.models.transformer"), "TransformerModel")
+            except ModuleNotFoundError:
+                print("Error importing fairseq. See sugoi-readme.txt.")
+                raise SystemExit
+            self.sugoi = self.model.from_pretrained(
+                f'{SUGOI_ROOT}/japaneseModel',
+                checkpoint_file='big.pretrain.pt',
+                source_lang='ja',
+                target_lang='en',
+                bpe='sentencepiece',
+                sentencepiece_model=f'{SUGOI_ROOT}/spmModels/spm.ja.nopretok.model',
+                #playing with params
+                unkpen=10,
+                no_repeat_ngram_size=4,
+                iter_decode_max_iter=15,
+                lenpen=0.1
+                )
 
     def _fileGenerator(self):
         for file in self.files:
@@ -51,14 +78,23 @@ class Translator:
 
     async def translate(self):
         for file in self._fileGenerator():
-            for entry in file.genTextContainers():
-                # Skip already translated text
-                if OVERWRITE_TEXT or not entry['enText']:
-                    text = textprocess.process(file, entry['jpText'], {"noNewlines": True})
-                    entry['enText'] = textprocess.process(file, await self.requestTl(text), {"lineLen": LINE_LENGTH, "replace": True}) # defer to default
+            if TARGET_MODEL == "deepl":
+                for entry in file.genTextContainers():
+                    # Skip already translated text
+                    if OVERWRITE_TEXT or not entry['enText']:
+                        text = textprocess.process(file, entry['jpText'], {"noNewlines": True})
+                        entry['enText'] = textprocess.process(file, await self.requestTl(text), {"lineLen": LINE_LENGTH, "replace": True}) # defer to default
+            elif TARGET_MODEL == "sugoi":
+                entries = list(file.genTextContainers())
+                textArray = [textprocess.process(file, entry['jpText'], {"noNewlines": True}) for entry in entries]
+                resultArray = self.sugoi.translate(textArray)
+                for idx, entry in enumerate(entries):
+                    if OVERWRITE_TEXT or not entry['enText']:
+                        entry['enText'] = textprocess.process(file, resultArray[idx], {"lineLen": LINE_LENGTH, "replace": True})
             file.save()
-        await self.client.close()
-        STOP.set_result(True)
+        if USING_SERVER:
+            await self.client.close()
+            STOP.set_result(True)
 
     async def requestTl(self, text: str):
         await self.client.send(json.dumps({
@@ -71,9 +107,22 @@ class Translator:
     def recvTl(self, tl):
         self.currentTl.set_result(tl['text'])
 
+async def sugoiTranslate():
+    if not Path(SUGOI_ROOT).joinpath("japaneseModel", "big.pretrain.pt").is_file():
+        print("Translation model not found. See sugoi-readme.txt")
+        raise SystemExit
+    tl = Translator()
+    print("\n")
+    await tl.translate()
 
 def main():
-    asyncio.run(startServer())
-    # #todo: start headless browser
+    global USING_SERVER
+    if TARGET_MODEL == "deepl":
+        USING_SERVER = True
+        asyncio.run(startServer())
+        # #todo: start headless browser
+    elif TARGET_MODEL == "sugoi":
+        USING_SERVER = False
+        asyncio.run(sugoiTranslate())
 
 main()

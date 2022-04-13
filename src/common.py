89896d2ec45b29e220ebce7a29b195a20f0a2e13
noccu <37862870+noccu@users.noreply.github.com>
2022-04-13 22:34:56 +0200

    Fix possible saving error with some paths
    Didn't affect usual usage.

--- src/common.py
+++ src/common.py
@@ -58,7 +58,7 @@ def readJson(file) -> dict:
         return json.load(f)
 
 def writeJsonFile(file, data):
-    os.makedirs(os.path.dirname(file), exist_ok=True)
+    os.makedirs(os.path.dirname(os.path.realpath(file)), exist_ok=True)
     with open(file, "w", encoding="utf8", newline="\n") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 

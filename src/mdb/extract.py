noccu/umamusu-translate

commit 704821cc964870215463ea9fa9c42900f884703a

Allow subdirs in mdb extracts

src/mdb/extract.py
@@ -55,7 +55,7 @@ def main():
                             specStmt = f"{stmt} WHERE {entry['specifier']} IN ({specval});"
                         else:
                             specStmt = f"{stmt} WHERE {entry['specifier']} = {specval};"
-                        extract(db, specStmt, Path(args.dst, filename))
+                        extract(db, specStmt, Path(args.dst, entry['table'] if entry.get("subdir") else "", filename))
                 else:
                     if args.file and entry['file'] != args.file: continue
                     extract(db, stmt, Path(args.dst, entry['file']))

src/mdb/import.py
@@ -22,7 +22,7 @@ def translator(args, entry: dict):
 
         print(f"Importing {file}...")
         try:
-            data = common.TranslationFile(Path(args.src, file + ".json"))
+            data = common.TranslationFile(args.src / entry['table'] if entry.get("subdir") else "" / file + ".json")
         except FileNotFoundError:
             raise StopIteration
 
@@ -33,7 +33,7 @@ def translator(args, entry: dict):
 
 def parseArgs():
     ap = common.Args("Imports translations to master.mdb", defaultArgs=False)
-    ap.add_argument("-src", default="translations/mdb", help="Import path")
+    ap.add_argument("-src", default="translations/mdb", type=Path, help="Import path")
     ap.add_argument("-dst", default=common.GAME_MASTER_FILE, help="Path to master.mdb file")
     ap.add_argument("-B", "--backup", action="store_true", help="Backup the master.mdb file")
     ap.add_argument("-R", "--restore", action="store_true", help="Restore the master.mdb file from backup")

commit 480be066c9ad3e3bd174565944a4a649c78b9420

Fix mdb import error
Regression in 704821cc because coding AFTER an all-nighter
is a terrible idea that I can't stop doing.

src/mdb/extract.py
@@ -32,7 +32,7 @@ def extract(db: sqlite3.Connection, stmt: str, savePath: Path):
 def parseArgs():
     ap = common.Args("Extracts master.mdb data for translation", defaultArgs=False)
     ap.add_argument("-src", default=common.GAME_MASTER_FILE, help="Path to master.mdb file")
-    ap.add_argument("-dst", default="translations/mdb", help="Extraction path")
+    ap.add_argument("-dst", default="translations/mdb", type=Path, help="Extraction path")
     ap.add_argument("--no-skill-data", action="store_true", help="Skip extracting skill data (requires nodeJS)")
     ap.add_argument("--no-text", action="store_true", help="Skip extracting standard text data")
     ap.add_argument("-f", "--file", help="Extract specific file name (as found in index.json)")
@@ -55,10 +55,10 @@ def main():
                             specStmt = f"{stmt} WHERE {entry['specifier']} IN ({specval});"
                         else:
                             specStmt = f"{stmt} WHERE {entry['specifier']} = {specval};"
-                        extract(db, specStmt, Path(args.dst, entry['table'] if entry.get("subdir") else "", filename))
+                        extract(db, specStmt, args.dst / (entry['table'] if entry.get("subdir") else "") / filename)
                 else:
                     if args.file and entry['file'] != args.file: continue
-                    extract(db, stmt, Path(args.dst, entry['file']))
+                    extract(db, stmt, args.dst / entry['file'])
         db.close()
     if not args.no_skill_data:
         print("Extracting skill data...")

src/mdb/import.py
@@ -22,7 +22,7 @@ def translator(args, entry: dict):
 
         print(f"Importing {file}...")
         try:
-            data = common.TranslationFile(args.src / entry['table'] if entry.get("subdir") else "" / file + ".json")
+            data = common.TranslationFile(args.src / (entry['table'] if entry.get("subdir") else "") / (file + ".json"))
         except FileNotFoundError:
             raise StopIteration
 
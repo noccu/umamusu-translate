fda652717134f6cf011f98022ae6398b38a78509
noccu <37862870+noccu@users.noreply.github.com>
2022-11-23 16:07:00 +0100

    Fix mingit install

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL
 SET /P usemingit=Download MinGit (~20MB) to enable use of update.bat for easy updating? Enter first letter [y]es, [n]o: 
-IF %usemingit% EQU y ( CALL :mingit ) ELSE ( ECHO Skipping MinGit. )
+IF /I %usemingit% EQU y ( CALL :mingit ) ELSE ( ECHO Skipping MinGit. )
 
 CALL run.bat install
 EXIT /B
@@ -10,9 +10,8 @@ REM FUNCTIONS
 
 :mingit
 IF NOT EXIST ".mingit" (
-    ECHO Installing MinGit (https://github.com/git-for-windows/git/releases)...
-    powershell -command "Start-BitsTransfer -Source https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/MinGit-2.38.1-64-bit.zip -Destination mingit.zip"
-    powershell -command "Expand-Archive mingit.zip .mingit"
+    ECHO Installing MinGit ^(https://github.com/git-for-windows/git/releases^)...
+    powershell -command "Start-BitsTransfer -Source https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/MinGit-2.38.1-64-bit.zip -Destination mingit.zip; Expand-Archive mingit.zip .mingit"
     del mingit.zip
 ) ELSE ( ECHO MinGit already installed. )
 EXIT /B 0

4b4296ecc95eec0e497fa1cbdf62ad0a4f61036e
noccu <37862870+noccu@users.noreply.github.com>
2022-11-24 09:16:25 +0100

    Fix git usage with no git init

--- install.bat
+++ install.bat
@@ -14,4 +14,14 @@ IF NOT EXIST ".mingit" (
     powershell -command "Start-BitsTransfer -Source https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/MinGit-2.38.1-64-bit.zip -Destination mingit.zip; Expand-Archive mingit.zip .mingit"
     del mingit.zip
 ) ELSE ( ECHO MinGit already installed. )
+CALL :initgit
+EXIT /B 0
+
+:initgit
+IF NOT EXIST ".git" (
+    ECHO Initializing git repo...
+    .mingit\mingw64\bin\git.exe clone --no-checkout https://github.com/noccu/umamusu-translate.git gittmp
+    powershell -command "mv gittmp\.git .git"
+    DEL /Q gittmp
+) ELSE ( ECHO Already a Git repo. )
 EXIT /B 0

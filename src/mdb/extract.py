b41cf34e3dbc533c9717b7c51165741b7d8877da
noccu <37862870+noccu@users.noreply.github.com>
2023-01-26 21:53:19 +0100

    Minor code adjust

--- src/mdb/extract.py
+++ src/mdb/extract.py
@@ -97,8 +97,7 @@ def main():
         from subprocess import run
         run(["node", "src/mdb/scripts/extract-skill-data.js", args.src], check=True)
         import textprocess
-        from types import SimpleNamespace
-        textprocess.processFiles(SimpleNamespace(src="translations/mdb/alt/skill-desc.json", lineLength=-1, targetLines=6, forceResize=True))
+        textprocess.processFiles(common.Args.fake(src="translations/mdb/alt/skill-desc.json", lineLength=-1, targetLines=6, forceResize=True))
 
 
 if __name__ == '__main__':

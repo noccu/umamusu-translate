noccu/umamusu-translate

commit 860e4a0a080d6bbb7904af4b6ea6ef481ca95457

Refactor storyid
Mainly to deal with the new sets in home dialogue.
This feels quite clunky but ok, worries for later :>

src/common.py
@@ -6,6 +6,7 @@ import sys
 from typing import Generator
 import regex
 from datetime import datetime, timezone
+from dataclasses import dataclass, astuple, asdict
 
 import UnityPy
 from UnityPy.files import ObjectReader
@@ -25,7 +26,7 @@ TARGET_TYPES = SUPPORTED_TYPES[:-1]  # Omit mdb
 NAMES_BLACKLIST = ["<username>", "", "モノローグ"]  # Special-use game names, don't touch
 
 
-def searchFiles(targetType, targetGroup, targetId, targetIdx=False, changed=False, jsonOnly=True) -> list[str]:
+def searchFiles(targetType, targetGroup, targetId, targetIdx=False, targetSet=False, changed=False, jsonOnly=True) -> list[str]:
     found = list()
     isJson = lambda f: PurePath(f).suffix == ".json" if jsonOnly else True
     if changed:
@@ -44,7 +45,9 @@ def searchFiles(targetType, targetGroup, targetId, targetIdx=False, changed=Fals
         searchDir = targetType if isinstance(targetType, os.PathLike) else os.path.join("translations", targetType)
         for root, dirs, files in os.walk(searchDir):
             depth = len(dirs[0]) if dirs else -1
-            if targetGroup and depth == 2:
+            if targetSet and depth == 5:
+                dirs[:] = [d for d in dirs if d == targetSet]
+            elif targetGroup and depth == 2:
                 dirs[:] = [d for d in dirs if d == targetGroup]
             elif targetId:
                 if targetType in ("lyrics", "preview"):
@@ -59,26 +62,66 @@ def searchFiles(targetType, targetGroup, targetId, targetIdx=False, changed=Fals
     return found
 
 
-# TODO: This is unpacking a string we packed ourselves, refactoring should eliminate this fn entirely.
-def parseStoryId(text_type, s) -> tuple:
-    if text_type in ("lyrics", "preview"):
-        return None, s, s
-    else:
-        return s[:2], s[2:6], s[6:]
-
-
-def parseStoryIdFromPath(text_type: str, path: str):
-    """Given a text type (story, lyrics, etc.) and a game data filepath, extract and return the group, id, and index."""
-    if text_type == "home":
-        path = path[-10:]
-        return path[:2], path[3:7], path[7:]
-    elif text_type == "lyrics":
-        return None, None, path[-11:-7]
-    elif text_type == "preview":
-        return None, None, path[-4:]
-    else:  # story and storyrace
-        path = path[-9:]
-        return path[:2], path[2:6], path[6:9]
+@dataclass
+class StoryId:
+    type:str = "story"
+    set:str = None
+    setLen = 5
+    group:str = None
+    groupLen = 2
+    id:str = None
+    idLen = 4
+    idx:str = None
+    idxLen = 3
+    def __post_init__(self):
+        if self.type in ("lyrics", "preview") and self.idx and not self.id:
+            self.id = self.idx
+    def __str__(self) -> str:
+        '''Return the joined numeric parts, as written in tlFiles'''
+        return "".join(x for x in astuple(self)[1:] if x is not None)
+    @classmethod
+    def parse(cls, text_type, s):
+        if text_type in ("lyrics", "preview"):
+            return cls(type=text_type, id=s)
+        elif len(s) > 9 and text_type == "home":
+            return cls(type=text_type, set=s[:5], group=s[5:7], id=s[7:11], idx=s[11:])
+        else:
+            return cls(type=text_type, group=s[:2], id=s[2:6], idx=s[6:])
+    @classmethod
+    def parseFromPath(cls, text_type: str, path: str):
+        """Given a text type (story, lyrics, etc.) and a game data filepath, extract and return the group, id, and index."""
+        if text_type == "home":
+            path = path[-16:]
+            return cls(type=text_type, set=path[:5], group=path[6:8], id=path[9:13], idx=path[13:])
+        elif text_type == "lyrics":
+            return cls(type=text_type, id=path[-11:-7])
+        elif text_type == "preview":
+            return cls(type=text_type, id=path[-4:])
+        else:  # story and storyrace
+            path = path[-9:]
+            return cls(type=text_type, group=path[:2], id=path[2:6], idx=path[6:9])
+    @classmethod
+    def queryfy(cls, storyId:'StoryId'):
+        '''Returns a new StoryId with attributes usable in SQL'''
+        parts = asdict(storyId)
+        for k,v in parts.items():
+            if v is None:
+                parts[k] = "_" * getattr(storyId, f"{k}Len", 0)
+        return cls(*parts.values())
+    @classmethod
+    def fromLegacy(cls, group, id, idx):
+        return cls(group=group, id=id, idx=idx)
+    def asLegacy(self):
+        return self.group, self.id, self.idx
+    def asTuple(self, validOnly=False):
+        if validOnly:
+            # Faster with the list comp for some extra mem cost
+            return tuple([x for x in astuple(self) if x is not None])
+        else:
+            return astuple(self)
+    def asPath(self, includeIdx=False):
+        offset = None if includeIdx else -1
+        return Path().joinpath(*self.asTuple(validOnly=True)[1:offset]) # ignore type for now
 
 
 def patchVersion():
@@ -103,6 +146,7 @@ class Args(argparse.ArgumentParser):
         if defaultArgs:
             self.add_argument("-t", "--type", choices=types or TARGET_TYPES, default=types[0] if types else TARGET_TYPES[0],
                               help="The type of assets to process.")
+            self.add_argument("-s", "--set", help="The set to process")
             self.add_argument("-g", "--group", help="The group to process")
             self.add_argument("-id", help="The id (subgroup) to process")
             self.add_argument("-idx", help="The specific asset index to process")
@@ -309,7 +353,7 @@ class TranslationFile:
         if not tlFile.fileExists:
             return
         if newName is None:
-            idx = parseStoryId(tlFile.getStoryId())[-1]
+            idx = StoryId.parse(tlFile.getStoryId()).idx
             title = tlFile.data.get('title')
             newName = f"{idx} ({title}).json" if title else f"{idx}.json"
         newName = Path(tlFile.file).parent.joinpath(helpers.sanitizeFilename(newName))

src/extract.py
@@ -6,33 +6,22 @@ from typing import Optional, Union
 from Levenshtein import ratio as similarity
 
 import common
-from common import GAME_META_FILE, GAME_ASSET_ROOT, TranslationFile, GameBundle
+from common import GAME_META_FILE, GAME_ASSET_ROOT, TranslationFile, GameBundle, StoryId
 from helpers import sanitizeFilename
 
-def queryfyStoryid(group, id, idx):
-    group = group or "__"
-    id = id or "____"
-    idx = idx or "___"
-    return group, id, idx
+def queryDB(db=None, storyId:StoryId=None):
+    storyId = StoryId.queryfy(storyId)
 
-def queryDB(db=None, storyId=None):
-    if storyId:
-        group, id, idx = queryfyStoryid(*common.parseStoryId(args.type, storyId))
-    else:
-        group, id, idx = queryfyStoryid(args.group, args.id, args.idx)
-
-    if args.type == "story":
-        pattern = f"{args.type}/data/{group}/{id}/{args.type}timeline%{idx}"
-    elif args.type == "home":
-        pattern = f"{args.type}/data/00000/{group}/{args.type}timeline_00000_{group}_{id}{idx}%"
-    elif args.type == "race":
-        pattern = f"{args.type}/storyrace/text/storyrace_{group}{id}{idx}%"
-    elif args.type == "lyrics":
-        if args.idx and not args.id: id = args.idx
-        pattern = f"live/musicscores/m{id}/m{id}_lyrics"
-    elif args.type == "preview":
-        if args.idx and not args.id: id = args.idx
-        pattern = f"outgame/announceevent/loguiasset/ast_announce_event_log_ui_asset_0{id}"
+    if storyId.type == "story":
+        pattern = f"{storyId.type}/data/{storyId.group}/{storyId.id}/{storyId.type}timeline%{storyId.idx}"
+    elif storyId.type == "home":
+        pattern = f"{storyId.type}/data/{storyId.set}/{storyId.group}/{storyId.type}timeline_{storyId.set}_{storyId.group}_{storyId.id}{storyId.idx}%"
+    elif storyId.type == "race":
+        pattern = f"{storyId.type}/storyrace/text/storyrace_{storyId.group}{storyId.id}{storyId.idx}%"
+    elif storyId.type == "lyrics":
+        pattern = f"live/musicscores/m{storyId.id}/m{storyId.id}_lyrics"
+    elif storyId.type == "preview":
+        pattern = f"outgame/announceevent/loguiasset/ast_announce_event_log_ui_asset_0{storyId.id}"
 
     externalDb = bool(db)
     if not externalDb:
@@ -45,7 +34,7 @@ def queryDB(db=None, storyId=None):
     return results
 
 
-def extractAsset(asset: GameBundle, storyId, tlFile=None) -> Union[None, TranslationFile]:
+def extractAsset(asset: GameBundle, storyId:StoryId, tlFile=None) -> Union[None, TranslationFile]:
     asset.load()
 
     if not asset.rootAsset.serialized_type.nodes:
@@ -89,7 +78,7 @@ def extractAsset(asset: GameBundle, storyId, tlFile=None) -> Union[None, Transla
             transferExisting(storyId, textData)
             export['text'].append(textData)
     else:
-        export['storyId'] = "".join(storyId) if args.type == "home" else tree['StoryId']
+        export['storyId'] = str(storyId) if args.type == "home" else tree['StoryId']
         export['title'] = tree['Title']
 
         for block in tree['BlockList']:
@@ -207,15 +196,14 @@ class DataTransfer:
             self._printedName = True
         print(text)
 
-    def __call__(self, storyId, textData):
+    def __call__(self, storyId:StoryId, textData):
         # Existing files are skipped before reaching here so there's no point in checking when we know the result already.
         # Only continue when forced to.
         if not args.overwrite or self.file == 0:
             return
-        group, id, idx = storyId
 
         if self.file is None:
-            file = next((Path(args.dst) / group / id).glob(f"{idx}*.json"), None)
+            file = next((Path(args.dst).joinpath(storyId.asPath())).glob(f"{storyId.idx}*.json"), None)
             if file is None:  # Check we actually found a file above
                 self.file = 0
                 return
@@ -294,7 +282,7 @@ def exportAsset(bundle: Optional[str], path: str, db=None):
             print(f"File already on latest version, skipping: {path}")
             return
 
-        storyId = tlFile.getStoryId()
+        storyId = StoryId.parse(args.type, tlFile.getStoryId())
         try:
             bundle, _ = queryDB(db, storyId)[0] #get the newest bundle hash/name
         except IndexError:
@@ -305,16 +293,15 @@ def exportAsset(bundle: Optional[str], path: str, db=None):
                 print(f"Bundle {bundle} not changed, skipping.")
             return
         print(f"{'Upgrading' if args.upgrade else 'Updating'} {bundle}")
-        group, id, idx = common.parseStoryId(args.type, storyId)
     else:  # path = unity internal, bundle = newest from SQL lookup
         tlFile = None
-        group, id, idx = common.parseStoryIdFromPath(args.type, path)
+        storyId = StoryId.parseFromPath(args.type, path)
 
-    exportDir = Path(args.dst) if args.type in ("lyrics", "preview") else Path(args.dst) / group / id
+    exportDir = Path(args.dst) if args.type in ("lyrics", "preview") else Path(args.dst).joinpath(storyId.asPath())
 
     # Skip if already exported and we're not overwriting
     if not args.overwrite:
-        file = next(exportDir.glob(f"{idx}*.json"), None)
+        file = next(exportDir.glob(f"{storyId.idx}*.json"), None)
         if file is not None:
             if args.verbose:
                 print(f"Skipping existing: {file.name}")
@@ -329,17 +316,16 @@ def exportAsset(bundle: Optional[str], path: str, db=None):
             print(f"Skipping patched asset: {asset.bundleName}")
         return
     try:
-        outFile = extractAsset(asset, (group, id, idx), tlFile)
+        outFile = extractAsset(asset, storyId, tlFile)
         if not outFile:
             return
     except:
-        print(f"Failed extracting bundle {bundle}, g {group}, id {id} idx {idx} to {exportDir}")
+        print(f"Failed extracting bundle {bundle}, g {storyId.group}, id {storyId.id} idx {storyId.idx} to {exportDir}")
         raise
 
     # Remove invalid path chars (win)
     title = sanitizeFilename(outFile.data.get('title', ''))
-    idxString = f"{idx} ({title})" if title else idx
-
+    idxString = f"{storyId.idx} ({title})" if title else storyId.idx
     outFile.setFile(str(exportDir / f"{idxString}.json"))
     outFile.save()
 
@@ -374,7 +360,7 @@ def main():
             for type in args.update: # set correctly by arg parsing
                 args.dst = PurePath("translations") / type
                 args.type = type
-                files = common.searchFiles(type, args.group, args.id, args.idx, changed=args.changed)
+                files = common.searchFiles(type, args.group, args.id, args.idx, set = args.set, changed=args.changed)
                 print(f"Found {len(files)} files for {type}.")
                 for i, file in enumerate(files):
                     try:
@@ -385,9 +371,9 @@ def main():
         finally:
             db.close()
     else:
-        print(f"Extracting group {args.group}, id {args.id}, idx {args.idx} (overwrite: {args.overwrite})\n"
+        print(f"Extracting type {args.type}, set {args.set}, group {args.group}, id {args.id}, idx {args.idx} (overwrite: {args.overwrite})\n"
               f"from {GAME_ASSET_ROOT} to {args.dst}")
-        q = queryDB()
+        q = queryDB(storyId=StoryId(args.type, args.set, args.group, args.id, args.idx))
         print(f"Found {len(q)} files.")
         for bundle, path in q:
             exportAsset(bundle, path)

src/edit_story.py
@@ -688,7 +688,7 @@ def main():
     if args.src:
         files = [args.src]
     else:
-        files = common.searchFiles(args.type, args.group, args.id, args.idx, changed = args.changed)
+        files = common.searchFiles(args.type, args.group, args.id, args.idx, targetSet=args.set, changed = args.changed)
         if not files:
             print("No files match given criteria")
             raise SystemExit

src/machinetl.py
@@ -36,7 +36,7 @@ async def startServer():
 
 class Translator:
     def __init__(self, client: server.WebSocketServerProtocol = None):
-        self.files = [args.src] if args.src else common.searchFiles(args.type, args.group, args.id, args.idx, changed = args.changed)
+        self.files = [args.src] if args.src else common.searchFiles(args.type, args.group, args.id, args.idx, targetSet=args.set, changed = args.changed)
 
         if USING_SERVER:
             self.client = client

src/names.py
@@ -56,7 +56,7 @@ def main():
         print("No names in given type.")
         raise SystemExit
 
-    files = args.src or common.searchFiles(args.type, args.group, args.id, args.idx, changed = args.changed)
+    files = args.src or common.searchFiles(args.type, args.group, args.id, args.idx, targetSet=args.set, changed = args.changed)
     if args.extract:
         n = extract(files)
         print(f"Extracted {n} new names from {len(files)} files.")

src/ruby-remover.py
@@ -3,15 +3,15 @@ import sqlite3
 from types import SimpleNamespace
 
 def removeRuby(args, db: sqlite3.Connection):
-    storyId = f"{args.group}{args.id}{args.idx}"
-    q = db.execute(f"select h, n from a where n like 'story/data/__/____/ast_ruby_{storyId}'")
+    storyId = common.StoryId(args.type, args.set, args.group, args.id, args.idx)
+
+    q = db.execute(f"select h, n from a where n like 'story/data/__/____/ast_ruby_{common.StoryId.queryfy(storyId)}'")
     patched = total = 0
     dummytl = SimpleNamespace(data=dict())
     for bundle, path in q:
         bundle = common.GameBundle.fromName(bundle, load = False)
         if bundle.isPatched:
-            storyId = "".join(common.parseStoryId("story", path))
-            if args.verbose: print(f"Skipping {storyId} ({bundle.bundleName}): Already patched")
+            if args.verbose: print(f"Skipping {common.StoryId.parseFromPath(path)} ({bundle.bundleName}): Already patched")
         else:
             bundle.load()
             tree = bundle.rootAsset.read_typetree()
@@ -28,9 +28,6 @@ def main():
     ap.add_argument("-dst", default=common.GAME_META_FILE, help="Path to master.mdb file")
     args = ap.parse_args()
     args.type = "story"
-    args.group =args.group or "__"
-    args.id = args.id or "____"
-    args.idx = args.idx or "___"
     try:
         with sqlite3.connect(args.dst, isolation_level=None) as db:
             db.execute("PRAGMA journal_mode = MEMORY;")

src/snep-import.py
@@ -6,7 +6,7 @@ from os import system as run
 snep = Path("translations/snep").glob("story_*")
 
 for file in snep:
-    g, id, idx = common.parseStoryIdFromPath("story", file.stem)
+    g, id, idx = common.StoryId.parseFromPath("story", file.stem)
 
     extract = common.searchFiles("story", g, id, idx)
     if not extract:

src/subtransfer.py
@@ -445,7 +445,7 @@ def createSubs(tlFile, fps = 30):
     return subs
 
 def writeSubs(sType, storyid):
-    files = common.searchFiles(sType, *common.parseStoryId(sType, storyid))
+    files = common.searchFiles(sType, *common.StoryId.parse(sType, storyid))
     for tlFile in files:
         tlFile = common.TranslationFile(tlFile)
         subs = createSubs(tlFile)

commit c759dee366e36f33ba5970a665e79d1cd3a7306a

Fix lyrics extraction
Regression in 860e4a0a

src/common.py
@@ -73,16 +73,20 @@ class StoryId:
     idLen = 4
     idx:str = None
     idxLen = 3
+    idOnlyGroup = ("lyrics", "preview")
     def __post_init__(self):
-        if self.type in ("lyrics", "preview") and self.idx and not self.id:
-            self.id = self.idx
+        if self.type in self.idOnlyGroup:
+            if not self.id and self.idx:
+                self.id = self.idx
             self.idx = None
+            self.group = None
+            self.set = None
     def __str__(self) -> str:
         '''Return the joined numeric parts, as written in tlFiles'''
         return "".join(x for x in astuple(self)[1:] if x is not None)
     @classmethod
     def parse(cls, text_type, s):
-        if text_type in ("lyrics", "preview"):
+        if text_type in cls.idOnlyGroup:
             return cls(type=text_type, id=s)
         elif len(s) > 9 and text_type == "home":
             return cls(type=text_type, set=s[:5], group=s[5:7], id=s[7:11], idx=s[11:])
@@ -116,13 +120,20 @@ class StoryId:
         return self.group, self.id, self.idx
     def asTuple(self, validOnly=False):
         if validOnly:
-            # Faster with the list comp for some extra mem cost
+            # Faster with the list comp for some extra mem cost, apparently
             return tuple([x for x in astuple(self) if x is not None])
         else:
             return astuple(self)
     def asPath(self, includeIdx=False):
         offset = None if includeIdx else -1
         return Path().joinpath(*self.asTuple(validOnly=True)[1:offset]) # ignore type for now
+    def getFilenameIdx(self):
+        if self.type in self.idOnlyGroup:
+            return self.id
+        elif self.idx:
+            return self.idx
+        else:
+            raise AttributeError
 
 
 def patchVersion():
@@ -338,7 +349,7 @@ class TranslationFile:
 
     def init(self, snapshot=True):
         self.version = self._getVersion()
-        self.escapeNewline = self.type in ("race", "preview", "mdb")
+        self.escapeNewline = self.type in ("race", "preview", "mdb", "lyrics")
         self.data['text'] = self.TextData(self)
         if snapshot: self.snapshot()
 

src/extract.py
@@ -301,7 +301,7 @@ def exportAsset(bundle: Optional[str], path: str, db=None):
 
     # Skip if already exported and we're not overwriting
     if not args.overwrite:
-        file = next(exportDir.glob(f"{storyId.idx}*.json"), None)
+        file = next(exportDir.glob(f"{storyId.getFilenameIdx()}*.json"), None)
         if file is not None:
             if args.verbose:
                 print(f"Skipping existing: {file.name}")
@@ -325,7 +325,7 @@ def exportAsset(bundle: Optional[str], path: str, db=None):
 
     # Remove invalid path chars (win)
     title = sanitizeFilename(outFile.data.get('title', ''))
-    idxString = f"{storyId.idx} ({title})" if title else storyId.idx
+    idxString = f"{storyId.idx} ({title})" if title else storyId.getFilenameIdx()
     outFile.setFile(str(exportDir / f"{idxString}.json"))
     outFile.save()
 
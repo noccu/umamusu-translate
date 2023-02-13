noccu/umamusu-translate

commit 31076db6732a08ba050afdfa721b6846b0664db6

Mark systext newline escapes correctly
Also edit and simplify text normalization in the editor. Should solve
some formatting issues on Mac and with copied text, particularly.

src/common.py
@@ -199,7 +199,6 @@ class TranslationFile:
         if load:
             if not file: raise RuntimeError("Attempting to load tlfile but no file provided.")
             self.setFile(file)
-            self.fileExists = True  # should error if it does not
             self.reload()
         else:
             self.fileExists = False
@@ -323,6 +322,7 @@ class TranslationFile:
 
     def reload(self):
         self.data = helpers.readJson(self.file)
+        self.fileExists = True  # should error if it does not
         self.init()
 
     def save(self):
@@ -350,6 +350,8 @@ class TranslationFile:
     def init(self, snapshot=True):
         self.version = self._getVersion()
         self.escapeNewline = self.type in ("race", "preview", "mdb", "lyrics")
+        if self.type == "mdb" and "system_text" in self.file:
+            self.escapeNewline = False
         self.data['text'] = self.TextData(self)
         if snapshot: self.snapshot()
 

src/edit_story.py
@@ -361,8 +361,8 @@ def load_block(event=None, dir=1):
 
 def save_block():
     if "enName" in cur_file.textBlocks[cur_block]:
-        cur_file.textBlocks[cur_block]['enName'] = cleanText(speaker_en_entry.get())
-    cur_file.textBlocks[cur_block]['enText'] = txt_for_display(text_box_en.get(1.0, tk.END), reverse=True)
+        cur_file.textBlocks[cur_block]['enName'] = normalizeEditorText(speaker_en_entry.get())
+    cur_file.textBlocks[cur_block]['enText'] = txt_for_storage(text_box_en.get(1.0, tk.END))
 
     # Get the new clip length from spinbox
     new_clip_length = block_duration_spinbox.get()
@@ -397,7 +397,7 @@ def next_block(event=None):
 
 def copy_block(event=None):
     root.clipboard_clear()
-    root.clipboard_append(cur_file.textBlocks[cur_block]['jpText'])
+    root.clipboard_append(txt_for_display(cur_file.textBlocks[cur_block]['jpText']))
 
 def loadFile(chapter=None):
     ch = chapter or cur_chapter
@@ -435,7 +435,7 @@ def close_text_list():
     for i, t in enumerate(extra_text_list_textboxes):
         jpBox, enBox = t
         if cur_text_list and i < len(cur_text_list):
-            cur_text_list[i]['enText'] = cleanText(enBox.get(1.0, tk.END))  # choice don't really need special handling
+            cur_text_list[i]['enText'] = normalizeEditorText(enBox.get(1.0, tk.END))  # choice don't really need special handling
         jpBox['state'] = 'normal'  # enable deletion...
         jpBox.delete(1.0, tk.END)
         enBox.delete(1.0, tk.END)
@@ -684,27 +684,22 @@ def process_text(event):
         proc_text = cur_file.textBlocks[cur_block].get("enText")
     else:
         opts["redoNewlines"] = True if event.state & 0x0001 else False
-        proc_text = textprocess.processText(cur_file, cleanText(text_box_en.get(1.0, tk.END)), opts)
+        proc_text = textprocess.processText(cur_file, normalizeEditorText(text_box_en.get(1.0, tk.END)), opts)
     text_box_en.delete(1.0, tk.END)
     text_box_en.insert(tk.END, proc_text)
     return "break"
 
 
-def txt_for_display(text, reverse=False):
+def txt_for_display(text):
     if cur_file.escapeNewline:
-        if reverse:
-            text = cleanText(text)
-            return text.replace("\n", "\\n")
-        else:
-            return text.replace("\\n", "\n")
+        return re.sub(r"(?:\\[rn])+", "\n", text)
     else:
-        if reverse:
-            text = cleanText(text)
-        return text
+        return text.replace("\r", "")
+def txt_for_storage(text):
+    return normalizeEditorText(text, "\\n" if cur_file.escapeNewline else "\n")
 
-
-def cleanText(text: str):
-    return " \n".join([line.strip() for line in text.strip().split("\n")])
+def normalizeEditorText(text: str, newline:str="\n"):
+    return f" {newline}".join([line.strip() for line in text.strip().split("\n")])
 
 def loadFont(fontPath):
     # code modified from https://github.com/ifwe/digsby/blob/f5fe00244744aa131e07f09348d10563f3d8fa99/digsby/src/gui/native/win/winfonts.py#L15

commit dc162a96a8880b4f9dd7e8a6d407fe384b6a8b41

Fix mdb import error
Regression in 31076db6
Unifies tlfile path to Path class.

src/common.py
@@ -3,7 +3,7 @@ import json
 import os
 from pathlib import Path, PurePath
 import sys
-from typing import Generator
+from typing import Generator, Union
 import regex
 from datetime import datetime, timezone
 from dataclasses import dataclass, astuple, asdict
@@ -194,7 +194,7 @@ class TranslationFile:
     ver_offset_mdb = 100
     textBlacklist = regex.compile(r"^タイトルコール$|イベントタイトルロゴ表示.*|※*ダミーテキスト|^欠番$")
 
-    def __init__(self, file=None, load=True, readOnly=False):
+    def __init__(self, file:Union[str, Path]=None, load=True, readOnly=False):
         self.readOnly = readOnly
         if load:
             if not file: raise RuntimeError("Attempting to load tlfile but no file provided.")
@@ -314,7 +314,7 @@ class TranslationFile:
             return self.data['storyId']
         else:
             isN = regex.compile(r"\d+")
-            g, id, idx = PurePath(self.file).parts[-3:]  # project structure provides at least 3 levels, luckily
+            g, id, idx = self.file.parts[-3:]  # project structure provides at least 3 levels, luckily
             if not isN.match(g): g = ""
             if not isN.match(id): id = ""
             idx = isN.match(idx)[0]
@@ -343,14 +343,14 @@ class TranslationFile:
         else:
             self._snapshot = json.dumps(self.data, ensure_ascii=False, default=helpers._to_json)
 
-    def setFile(self, file):
-        self.file = file
-        self.name = PurePath(file).name
+    def setFile(self, file:Union[str, Path]):
+        self.file = Path(file)
+        self.name = self.file.name
 
     def init(self, snapshot=True):
         self.version = self._getVersion()
         self.escapeNewline = self.type in ("race", "preview", "mdb", "lyrics")
-        if self.type == "mdb" and "system_text" in self.file:
+        if self.type == "mdb" and self.file.parent.name == "character_system_text":
             self.escapeNewline = False
         self.data['text'] = self.TextData(self)
         if snapshot: self.snapshot()
@@ -370,8 +370,8 @@ class TranslationFile:
             idx = StoryId.parse(tlFile.type, tlFile.getStoryId()).idx
             title = tlFile.data.get('title')
             newName = f"{idx} ({title}).json" if title else f"{idx}.json"
-        newName = Path(tlFile.file).parent.joinpath(helpers.sanitizeFilename(newName))
-        os.rename(tlFile.file, newName)
+        newName = tlFile.file.parent.joinpath(helpers.sanitizeFilename(newName))
+        tlFile.file.rename(newName)
         tlFile.setFile(newName)
 
 

src/manage.py
@@ -144,13 +144,13 @@ def convertMdb():
     print(f"Converted {len(converted)} files.")
     return converted
 
-def convertTlFile(file: common.TranslationFile, overwrite=False):
+def convertTlFile(tlFile: common.TranslationFile, overwrite=False):
     converted = list()
-    path = LOCALIFY_DATA_DIR / file.type / (file.getStoryId() + ".json")
-    if not overwrite and path.exists() and path.stat().st_mtime >= Path(file.file).stat().st_mtime:
+    path = LOCALIFY_DATA_DIR / tlFile.type / (tlFile.getStoryId() + ".json")
+    if not overwrite and path.exists() and path.stat().st_mtime >= tlFile.file.stat().st_mtime:
         return
     data = dict()
-    for b in file.genTextContainers():
+    for b in tlFile.genTextContainers():
         if text := b.get('enText'):
             data[TextHasher.hash(b['jpText'])] = TextHasher.normalize(text)
         if name := b.get('enName'):
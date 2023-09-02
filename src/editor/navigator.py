noccu/umamusu-translate

commit d869d80c9445820d9a86c07602f5c34f779f42ac

Minimal working file split + class layout
A lot of changes at once... there is still a lot more to do, and no
this layout isn't final because frankly it sucks, I'm learning :>
Yes I should have taken it bit by bit to prevent bugs and have
a smooth experience with history, but it's too late now :)

This actually fixes block dropdown not updating on chapter change.
Otherwise functionality is exactly the same, apart from bugs (:

src/editor/navigator.py
@@ -1,0 +1,157 @@
+import tkinter as tk
+from tkinter import ttk
+from typing import TYPE_CHECKING
+
+import textprocess
+from common.constants import NAMES_BLACKLIST
+from common.utils import isEnglish
+
+if TYPE_CHECKING:
+    from common.types import TranslationFile
+
+    from .main import Editor
+
+
+class Navigator:
+    def __init__(self, master: "Editor") -> None:
+        # todo: turn this class into a frame that can be composited in
+        chapter_label = tk.Label(master.root, text="Chapter")
+        chapter_label.grid(row=0, column=0)
+        chapter_dropdown = ttk.Combobox(master.root, width=35)
+        chapter_dropdown.bind("<<ComboboxSelected>>", self.change_chapter)
+        chapter_dropdown.bind("<KeyRelease>", self.searchChapters)
+        chapter_dropdown.config(values=("No files loaded",))
+        chapter_dropdown.current(0)
+        chapter_dropdown.search = None
+        chapter_dropdown.grid(row=0, column=1, sticky=tk.NSEW)
+
+        textblock_label = tk.Label(master.root, text="Block")
+        textblock_label.grid(row=0, column=2)
+        block_dropdown = ttk.Combobox(master.root, width=35)
+        block_dropdown.bind("<<ComboboxSelected>>", self.change_block)
+        block_dropdown.config(values=("No file loaded",))
+        block_dropdown.current(0)
+        block_dropdown.grid(row=0, column=3, sticky=tk.NSEW)
+
+        self.cur_chapter = 0
+        self.cur_block = 0
+        self.cur_data = None
+        self.cur_file: "TranslationFile" = None
+        self.chapterPicker = chapter_dropdown
+        self.blockPicker = block_dropdown
+        self.master = master
+
+    def change_chapter(self, event=None):
+        if self.cur_file:
+            self.master.fileMan.save_block(self)
+        if self.chapterPicker.search:
+            cur_chapter = self.chapterPicker.search[self.chapterPicker.current()]
+            self.resetChapterSearch()
+        else:
+            cur_chapter = self.chapterPicker.current()
+
+        cur_file = self.master.fileMan.loadFile(cur_chapter)
+        self.cur_file = cur_file
+        self.cur_chapter = cur_chapter
+        self.cur_block = 0
+        self.blockPicker["values"] = [
+            f"{i+1} - {block['jpText'][:16]}" for i, block in enumerate(cur_file.textBlocks)
+        ]
+        self.blockPicker.current(0)
+        self.cur_data = self.master.fileMan.load_block(self.cur_file, self.cur_block)
+
+        ll = textprocess.calcLineLen(cur_file, False) or self.master.textBoxEn.DEFAULT_WIDTH
+        self.master.textBoxEn.config(width=ll)
+        self.master.textBoxJp.config(width=ll)
+
+    def reload_chapter(self, event=None):
+        self.cur_file.reload()
+        self.cur_data = self.master.fileMan.load_block(self.cur_file, self.cur_block)
+
+    def prev_ch(self, event=None):
+        if self.cur_chapter - 1 > -1:
+            self.chapterPicker.current(self.cur_chapter - 1)
+            self.change_chapter()
+        else:
+            print("Reached first chapter")
+
+    def next_ch(self, event=None):
+        #! ugh
+        if self.cur_chapter + 1 < len(self.master.fileMan.files):
+            self.chapterPicker.current(self.cur_chapter + 1)
+            self.change_chapter()
+        else:
+            print("Reached last chapter")
+
+    def change_block(self, event=None, dir=1):
+        self.master.fileMan.save_block(self)
+
+        # ? Hope this doesn't loop but it didn't before so...?
+        if self.master.options.skip_translated.get():
+            blocks = self.cur_file.textBlocks
+            targetBlock = self.cur_block
+            data = blocks[self.cur_block]
+            while 0 < targetBlock < len(blocks) - 1 and (
+                data["enText"] or isEnglish(data["jpText"])
+            ):
+                targetBlock += dir
+                data = self.cur_file.textBlocks[targetBlock]
+            self.cur_block = targetBlock
+            self.blockPicker.current(targetBlock)
+
+        if self.master.options.saveOnBlockChange.get():
+            self.master.fileMan.saveFile()
+
+        self.cur_block = self.blockPicker.current()
+        self.cur_data = self.master.fileMan.load_block(self.cur_file, self.cur_block)
+
+    def prev_block(self, event=None):
+        if self.cur_block - 1 > -1:
+            self.blockPicker.current(self.cur_block - 1)
+            self.change_block(dir=-1)
+
+    def next_block(self, event=None):
+        next_index = self.cur_data.get("nextBlock", self.cur_block + 2) - 1
+        if next_index < 1 or next_index >= len(self.cur_file.textBlocks):
+            next_index = -1
+        # todo: following buttons are defined on self by Editor
+        if next_index > 0:
+            self.btnNext["state"] = "normal"
+            self.btnNext["text"] = f"Next ({next_index + 1})"
+        else:
+            self.btnNext["state"] = "disabled"
+            self.btnNext["text"] = "Next"
+        if next_index != -1:
+            self.blockPicker.current(next_index)
+            self.change_block()
+        else:
+            print("Reached end of chapter")
+
+    def searchChapters(self, event=None):
+        if event.keysym in ("Up", "Down", "Left", "Right", "Return"):
+            return
+        self.chapterPicker = 5  #! ???
+        search = self.chapterPicker.get()
+        if search == "":
+            self.chapterPicker["values"] = self.master.fileMan.formattedFiles
+            self.chapterPicker.search = None
+        else:
+            searchList = {
+                item: i
+                for i, item in enumerate(self.master.fileMan.formattedFiles)
+                if search in item
+            }
+            self.chapterPicker["values"] = (
+                list(searchList.keys()) if searchList else ["No matches found"]
+            )
+            self.chapterPicker.search = list(searchList.values()) if searchList else None
+
+    def resetChapterSearch(self):
+        self.chapterPicker["values"] = self.master.fileMan.formattedFiles
+        self.chapterPicker.search = None
+
+    def nextMissingName(self):
+        for idx, block in enumerate(self.cur_file.textBlocks):
+            if not block.get("enName") and block.get("jpName") not in NAMES_BLACKLIST:
+                self.blockPicker.current(idx)
+                self.change_block()

src/editor/display.py
@@ -1,0 +1,121 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.font import Font
+
+from common import constants as const
+
+if const.IS_WIN:
+    from ctypes import byref, create_string_buffer, create_unicode_buffer, windll
+
+
+class FontManager:
+    fontLoaded = False
+
+    def __init__(self, master) -> None:
+        if not FontManager.fontLoaded:
+            self.loadFont(r"src/data/RodinWanpakuPro-UmaTl.otf")
+        FontManager.fontLoaded = True
+
+        # Todo: consider adding general functions and defining these externally through them
+        self.FONT_LARGE = FONT_LARGE = Font(
+            master.root, family="RodinWanpakuPro UmaTl B", size=18, weight="normal"
+        )
+        self.FONT_BOLD = FONT_LARGE.copy()
+        self.FONT_BOLD.config(weight="bold")
+        self.FONT_ITALIC = FONT_LARGE.copy()
+        self.FONT_ITALIC.config(slant="italic")
+
+    def loadFont(self, fontPath):
+        # code modified from https://github.com/ifwe/digsby/blob/f5fe00244744aa131e07f09348d10563f3d8fa99/digsby/src/gui/native/win/winfonts.py#L15
+        if not const.IS_WIN:
+            print(
+                "Non-Windows system: Fonts, lengths, and previews won't match game.\n"
+                "To load custom game font install 'src/data/RodinWanpakuPro-B-ex.otf' to system fonts."
+            )
+            return
+        # origFontList = list(tk.font.families())
+        if isinstance(fontPath, bytes):
+            pathbuf = create_string_buffer(fontPath)
+            AddFontResourceEx = windll.gdi32.AddFontResourceExA
+        elif isinstance(fontPath, str):
+            pathbuf = create_unicode_buffer(fontPath)
+            AddFontResourceEx = windll.gdi32.AddFontResourceExW
+        else:
+            raise TypeError("fontPath must be bytes or str")
+
+        flags = 0x10 | 0x20  # private and not enumerable
+        # flags = 0x10 | 0 # private and enumerable
+
+        numFontsAdded = AddFontResourceEx(byref(pathbuf), flags, 0)
+        # print(
+        #   f"added {numFontsAdded} fonts:",
+        #   [name for name in tk.font.families() if name not in origFontList]
+        # )
+        # print(tk.font.families()[-3:])
+
+        return numFontsAdded
+
+
+class PopupMenu(tk.Menu):
+    def clear(self):
+        self.delete(0, tk.END)
+
+    def show(self, event: tk.Event, atInsert=False):
+        if atInsert:
+            x1, y1 = event.widget.bbox(tk.INSERT)[:2]
+            x = event.widget.winfo_rootx() + x1
+            y = event.widget.winfo_rooty() + y1
+        else:
+            x = event.x_root
+            y = event.y_root
+        try:
+            self.tk_popup(x, y)
+        finally:
+            self.grab_release()
+
+
+class ScrollableFrame:
+    def __init__(self, parent: tk.Toplevel):
+        scroll_frame = ttk.Frame(parent)
+        scroll_frame.pack(fill="both", expand=True)
+
+        scroll_canvas = tk.Canvas(scroll_frame)
+        scroll_canvas.pack(side="left", fill="both", expand=True)
+
+        scroll_bar = ttk.Scrollbar(scroll_frame, orient="vertical", command=scroll_canvas.yview)
+        scroll_bar.pack(side="right", fill="y")
+
+        scroll_canvas.configure(yscrollcommand=scroll_bar.set)
+        scroll_canvas.bind(
+            "<Configure>", lambda e: scroll_canvas.configure(scrollregion=scroll_canvas.bbox("all"))
+        )
+        window_frame = ttk.Frame(scroll_canvas)
+        scroll_canvas.create_window((0, 0), window=window_frame, anchor="nw")
+
+        scroll_canvas.bind_all("<MouseWheel>", self.scroll)
+        window_frame.bind("<Enter>", self.toggle)
+        window_frame.bind("<Leave>", self.toggle)
+
+        self.canvas = scroll_canvas
+        self.isScrollable = False
+        self.content = window_frame
+
+    def toggle(self, e):
+        self.isScrollable = not self.isScrollable
+
+    def scroll(self, e):
+        if self.isScrollable:
+            self.canvas.yview_scroll(-1 * int(e.delta / 35), "units")
+
+
+def _switchWidgetFocusForced(e):
+    e.widget.tk_focusNext().focus()
+    return "break"
+
+
+def setActive(widget: tk.Widget, active: bool):
+    widget["state"] = "normal" if active else "disabled"
+
+
+def setText(widget: tk.Widget, text: str):
+    widget["text"] = text

src/editor/files.py
@@ -1,0 +1,179 @@
+from tkinter import messagebox
+from typing import TYPE_CHECKING, Union
+
+from common import patch
+from common.constants import NAMES_BLACKLIST
+from common.types import StoryId, TranslationFile
+
+from . import display, text
+
+if TYPE_CHECKING:
+    from .main import Editor
+    from .navigator import Navigator
+
+
+class SaveState:
+    lastEnText: str
+    unsavedChanges = set()
+
+    def markBlockLoaded(self, block: dict):
+        self.lastEnText = block.get("enText")
+
+    def markBlockSaved(self, chapter: int, block: dict):
+        text = block.get("enText")
+        # short the str comp when changes already known
+        if chapter not in self.unsavedChanges and text != self.lastEnText:
+            self.unsavedChanges.add(chapter)
+
+    def onFileSaved(self, ch: int, block: dict):
+        # Little hack to prevent false unsaved files on chapter change without block change
+        # Essentially pretend the block was reloaded. Could be done in markBlockSaved but
+        # would usually be useless and immediately replaced by the actual block load
+        self.markBlockLoaded(block)
+        self.unsavedChanges.discard(ch)
+
+
+class FileManager:
+    def __init__(self, master: "Editor") -> None:
+        self.master = master
+        self.saveState = SaveState()
+        self.formattedFiles = None
+
+    def importFiles(self, src: Union[StoryId, str, list[Union[TranslationFile, str]]]):
+        if isinstance(src, StoryId):
+            # todo: make search accept storyid
+            files = patch.searchFiles(
+                src.type, src.group, src.id, src.idx, targetSet=src.set, changed=src.changed
+            )
+            if not files:
+                raise FileNotFoundError("No files match given criteria")
+        elif isinstance(src, str):
+            files = [src]
+        elif isinstance(src, list):
+            if not isinstance(src[0], (str, TranslationFile)):
+                raise ValueError
+            files = src
+        else:
+            raise NotImplementedError
+        files.sort()
+        # todo: Was chapter_dropdown.formattedList
+        self.files = files
+        self.formattedFiles = [f.split("\\")[-1] for f in files]
+        #! sigh
+        self.master.nav.chapterPicker["values"] = self.formattedFiles
+        self.master.nav.chapterPicker.current(0)
+        self.master.nav.change_chapter()
+        # reset savestate and others?
+
+    def loadFile(self, chapter: int) -> TranslationFile:
+        file = self.files[chapter]
+        if isinstance(file, str):
+            file = TranslationFile(file)
+            self.files[chapter] = file
+        return file
+
+    def saveFile(self, nav: "Navigator", saveAll=None):
+        self.save_block(nav)
+        targets = self.files if saveAll else (nav.cur_file,)
+
+        for ch, file in enumerate(targets):
+            if saveAll and isinstance(file, str):
+                continue
+            if self.master.options.markHuman.get():
+                file.data["humanTl"] = True
+            file.save()
+            # Prevent message spam
+            if not self.master.options.saveOnBlockChange.get() and not saveAll:
+                print("Saved")
+            self.saveState.onFileSaved(ch if saveAll else nav.cur_chapter, nav.cur_data)
+        if saveAll:
+            print("Saved all files")
+
+    def load_block(self, file: TranslationFile, block: int):
+        data = file.textBlocks[block]
+
+        # Fill in the text boxes
+        text.setText(self.master.speakerJp, data.get("jpName", ""))
+        if data.get("jpName") in NAMES_BLACKLIST:
+            text.clear(self.master.speakerEn)
+            display.setActive(self.master.speakerEn, False)
+        else:
+            display.setActive(self.master.speakerEn, True)
+            en_name = data.get("enName", "")
+            text.setText(self.master.speakerEn, en_name)
+            if en_name:
+                self.master.speakerEn.config(bg=self.master.COLOR_WIN)
+            else:
+                self.master.speakerEn.config(bg="red")
+
+        # Spinbox for text block duration
+        text.clear(self.master.blockDuration)
+        if "origClipLength" in data:
+            self.master.blockDurationLabel.config(text=f"Text Duration ({data['origClipLength']})")
+        if "newClipLength" in data:
+            self.master.blockDuration.insert(0, data["newClipLength"])
+        else:
+            if "origClipLength" in data:
+                self.master.blockDuration.insert(0, data["origClipLength"])
+            else:
+                self.master.blockDuration.insert(0, "-1")
+
+        display.setActive(self.master.textBoxJp, True)
+        text.setText(self.master.textBoxJp, text.for_display(file, data["jpText"]))
+        display.setActive(self.master.textBoxJp, False)
+        displayText = text.for_display(file, data["enText"])
+        self.master.textBoxEn.loadRichText(displayText)
+
+        # Update choices button
+        cur_choices = data.get("choices")
+        if cur_choices:
+            self.master.extraText.cur_choices = cur_choices
+            display.setActive(self.master.btnChoices, True)
+            self.master.btnChoices.config(bg="#00ff00")
+            self.master.extraText.toggle(allowShow=False, target=cur_choices)
+        else:
+            display.setActive(self.master.btnChoices, False)
+            self.master.btnChoices.config(bg=self.master.COLOR_BTN)
+
+        # Update colored button
+        cur_colored = data.get("coloredText")
+        if cur_colored:
+            self.master.extraText.cur_colored = cur_colored
+            display.setActive(self.master.btnColored, True)
+            self.master.btnColored.config(bg="#00ff00")
+            self.master.extraText.toggle(allowShow=False, target=cur_colored)
+        else:
+            display.setActive(self.master.btnColored, False)
+            self.master.btnColored.config(bg=self.master.COLOR_BTN)
+        self.saveState.markBlockLoaded(data)
+        self.master.spell_checker.check_spelling()
+        self.master.preview.setText(displayText)
+        return data
+
+    def save_block(self, nav: "Navigator"):
+        cur_file = nav.cur_file
+        cur_data = nav.cur_data
+
+        if "enName" in cur_data:
+            cur_data["enName"] = text.normalize(self.master.speakerEn.get())
+        cur_data["enText"] = text.for_storage(cur_file, self.master.textBoxEn.toRichText())
+
+        # Get the new clip length from spinbox
+        new_clip_length = self.master.blockDuration.get()
+        if new_clip_length.isnumeric():
+            new_clip_length = int(new_clip_length)
+            if "origClipLength" in cur_data and new_clip_length != cur_data["origClipLength"]:
+                cur_data["newClipLength"] = new_clip_length
+            else:
+                cur_data.pop("newClipLength", None)
+                if "origClipLength" not in cur_data:
+                    messagebox.showwarning(
+                        master=self.master.blockDuration,
+                        title="Cannot save clip length",
+                        message="This text block does not have an original clip length defined"
+                        " and thus cannot save a custom clip length. Resetting to -1.",
+                    )
+                    text.setText(self.master.blockDuration, "-1")
+        elif new_clip_length != "-1":
+            cur_data.pop("newClipLength", None)
+        self.saveState.markBlockSaved(nav.cur_chapter, cur_data)

src/editor/text.py
@@ -1,0 +1,190 @@
+import re
+import tkinter as tk
+from tkinter import colorchooser
+from tkinter.font import Font
+from typing import Union
+
+import textprocess
+
+TkTextBox = Union[tk.Text, tk.Entry]
+
+
+class ColorManager:
+    def __init__(self, master) -> None:
+        self.LAST_COLOR = None
+        self.master = master
+
+    def pick(self, useLast=True):
+        if not useLast or not self.LAST_COLOR:
+            self.LAST_COLOR = colorchooser.askcolor()[1]  # 0 = rgb tuple, 1=hex str
+            self.define(self.LAST_COLOR)
+        return self.LAST_COLOR
+
+    # todo: this wrote to en_box, adjust calls
+    def define(self, color: str):
+        # self.master.tag_config(f"color={color}", foreground=color)
+        tk.Text.tag_config(self.master, f"color={color}", foreground=color)
+
+
+class TextEditBox(tk.Text):
+    """A tk Text widget with extra features for editing"""
+
+    DEFAULT_WIDTH = 54
+    DEFAULT_HEIGHT = 4
+
+    # todo: maybe move color to post_init and pass through init itself to tk
+    def __init__(
+        self, parent, size: tuple[int] = None, editable: bool = True, font: Font = None
+    ) -> None:
+        if not size:
+            size = None, None
+        w = size[0] or TextEditBox.DEFAULT_WIDTH
+        h = size[1] or TextEditBox.DEFAULT_HEIGHT
+        super().__init__(
+            parent,
+            width=w,
+            height=h,
+            undo=True,
+            font=font,
+            state="normal" if editable else "disabled",
+        )
+        self.color = ColorManager(self)
+
+    def format_text(self, event):
+        if not self.tag_ranges("sel"):
+            print("No selection to format.")
+            return
+        currentTags = self.tag_names(tk.SEL_FIRST)
+        if event.keysym == "i":
+            if "i" in currentTags:
+                self.tag_remove("i", tk.SEL_FIRST, tk.SEL_LAST)
+            else:
+                self.tag_add("i", tk.SEL_FIRST, tk.SEL_LAST)
+        elif event.keysym == "b":
+            if "b" in currentTags:
+                self.tag_remove("b", tk.SEL_FIRST, tk.SEL_LAST)
+            else:
+                self.tag_add("b", tk.SEL_FIRST, tk.SEL_LAST)
+        elif event.keysym == "C":
+            color = f"color={self.color.pick(not (event.state & 131072))}"  # alt
+            if color is None:
+                return
+            if color in currentTags:
+                self.tag_remove(color, tk.SEL_FIRST, tk.SEL_LAST)
+            else:
+                self.tag_add(color, tk.SEL_FIRST, tk.SEL_LAST)
+        else:
+            return
+        return "break"  # prevent control char entry
+
+    def del_word(self, event):
+        pos = self.index(tk.INSERT)
+        start = "linestart" if event.state & 0x0001 else "wordstart"
+        end = "lineend" if event.state & 0x0001 else "wordend"
+        if event.keycode == 8:
+            self.delete(f"{pos} -1c {start}", pos)
+        elif event.keycode == 46:
+            self.delete(pos, f"{pos} {end}")
+
+    def char_convert(self, event=None):
+        pos = self.index(tk.INSERT)
+        start = pos + "-6c"
+        txt = self.get(start, pos)
+        m = re.search(r"[A-Z0-9]+", txt)
+        if m:
+            try:
+                res = chr(int(m.group(0), 16))
+            except ValueError:
+                return
+            self.replace(f"{start}+{str(m.start())}c", pos, res)
+
+    # todo: was insertTaggedTextFromMarkup
+    def loadRichText(self, text: str = None):
+        """Load text into widget, converting unity RT markup to tk tags.
+        If no text is given it converts all existing text"""
+        if text is None:
+            text = self.get(1.0, tk.END)
+        tagList = list()
+        offset = 0
+        openedTags = dict()
+        tagRe = r"<(/?)(([a-z]+)(?:[=#a-z\d]+)?)>"
+        for m in re.finditer(tagRe, text, flags=re.IGNORECASE):
+            isClose, fullTag, tagName = m.groups()
+            if tagName not in ("color", "b", "i", "size"):
+                continue
+            if isClose:
+                openedTags[tagName]["end"] = m.start() - offset
+            else:
+                tagList.append({"name": fullTag, "start": m.start() - offset})
+                openedTags[tagName] = tagList[-1]
+                if tagName == "color":
+                    self.color.define(fullTag.split("=")[-1])
+            offset += len(m[0])
+        # Add the cleaned text
+        self.delete(1.0, tk.END)
+        self.insert(tk.END, re.sub(tagRe, "", text, flags=re.IGNORECASE))
+        # Apply tags
+        for toTag in tagList:
+            self.tag_add(toTag["name"], f"1.0+{toTag['start']}c", f"1.0+{toTag['end']}c")
+
+    # todo: was tagsToMarkup
+    def toRichText(self):
+        text = list(self.get(1.0, tk.END))
+        offset = 0
+        tagList = list()
+        for tag in self.tag_names():
+            tagBaseName = tag.split("=")[0]
+            if tagBaseName not in ("i", "b", "color", "size"):
+                continue
+            ranges = self.tag_ranges(tag)
+            tagList.extend((text_count(self, "1.0", x, "-chars"), f"<{tag}>") for x in ranges[0::2])
+            tagList.extend(
+                (text_count(self, "1.0", x, "-chars"), f"</{tagBaseName}>") for x in ranges[1::2]
+            )
+        tagList.sort(key=lambda x: x[0])
+        for idx, tag in tagList:
+            text.insert(idx + offset, tag)
+            offset += 1
+        return "".join(text)
+
+    def process_text(self, file, block, event):
+        opts = {"redoNewlines": False, "replaceMode": "limit", "lineLength": -1, "targetLines": 99}
+        if getattr(event, "all", None):
+            opts["lineLength"] = 0
+            for block in file.textBlocks:
+                block["enText"] = textprocess.processText(file, block["enText"], opts)
+            proc_text = file.textBlocks[block].get("enText")
+        else:
+            opts["redoNewlines"] = True if event.state & 0x0001 else False
+            proc_text = textprocess.processText(file, normalize(self.get(1.0, tk.END)), opts)
+        setText(self, proc_text)
+        return "break"
+
+
+# https://github.com/python/cpython/issues/97928
+def text_count(widget, index1, index2, *options):
+    return widget.tk.call((widget._w, "count") + options + (index1, index2))
+
+
+def for_display(file, text):
+    if file.escapeNewline:
+        return re.sub(r"(?:\\[rn])+", "\n", text)
+    else:
+        return text.replace("\r", "")
+
+
+def for_storage(file, text):
+    return normalize(text, "\\n" if file.escapeNewline else "\n")
+
+
+def normalize(text: str, newline: str = "\n"):
+    return f" {newline}".join([line.strip() for line in text.strip().split("\n")])
+
+
+def setText(widget: TkTextBox, text: str):
+    clear(widget)
+    widget.insert(0 if isinstance(widget, tk.Entry) else 1.0, text)
+
+
+def clear(widget: TkTextBox):
+    widget.delete(0 if isinstance(widget, tk.Entry) else 1.0, tk.END)

commit a577b2d0eb1194249425e9d5b6b7bfe67731b040

Fix chapter search
Regression in d869d80c9445820d9a86c07602f5c34f779f42ac.

src/editor/navigator.py
@@ -130,7 +130,6 @@ class Navigator:
     def searchChapters(self, event=None):
         if event.keysym in ("Up", "Down", "Left", "Right", "Return"):
             return
-        self.chapterPicker = 5  #! ???
         search = self.chapterPicker.get()
         if search == "":
             self.chapterPicker["values"] = self.master.fileMan.formattedFiles
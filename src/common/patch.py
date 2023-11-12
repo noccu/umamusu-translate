afce1eb4806d0f1a5907aab9fb6c987cc272eb72
noccu <37862870+noccu@users.noreply.github.com>
2023-11-13 00:38:29 +0100

    Fix possible errors in file search
    Regression in d0ecfc840dbe3f473063ff003ff5faedde8912b3.
    Also small shortcut when traversing dirs with no files.

--- src/common/patch.py
+++ src/common/patch.py
@@ -65,10 +65,10 @@ def searchFiles(
     for root, dirs, files in os.walk(searchDir):
         root = Path(root)
         dirType = len(dirs[0]) if dirs else -1
-        if targetSet and dirType == 5 and targetSet in dirs:
-            dirs[:] = (targetSet,)
-        elif targetGroup and dirType == 2 and targetGroup in dirs:
-            dirs[:] = (targetGroup,)
+        if targetSet and dirType == 5:
+            dirs[:] = [d for d in dirs if d == targetSet]
+        elif targetGroup and dirType == 2:
+            dirs[:] = [d for d in dirs if d == targetGroup]
         elif targetId:
             if targetType in ("lyrics", "preview"):
                 found.extend(
@@ -77,9 +77,11 @@ def searchFiles(
                     if PurePath(file).stem == targetId and (utils.isJson(file) if jsonOnly else True)
                 )
                 continue  #? probably return
-            elif dirType == 4 and targetId in dirs:
-                dirs[:] = (targetId,)
-        if targetIdx and files:
+            elif dirType == 4:
+                dirs[:] =  [d for d in dirs if d == targetId]
+        if not files:
+            continue
+        if targetIdx:
             found.extend(
                 root.joinpath(file)
                 for file in files

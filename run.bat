d9e2b33a16be3a6ea5d558c52469d913554ba764
noccu <37862870+noccu@users.noreply.github.com>
2024-04-07 02:51:36 +0200

    Consolidate patching to one batch script

--- run.bat
+++ run.bat
@@ -17,27 +17,46 @@ ECHO Using %snek%
 %snek% -m pip show unitypy  | findstr /B "Version Location"
 
 REM Newline
-ECHO(
+ECHO.
 
 REM Treat first arg as label call if exists.
-IF [%1] NEQ [] ( GOTO %1 )
-
-:open
-CALL update.bat
-@REM %snek% src/filecopy.py --backup
-ECHO Importing all translatable types that are present in your game files...
-ECHO By default, already patched files are skipped. To forcefully rewrite all files, set update to false in the config.
-REM Or manually import parts, see import.py -h
-%snek% src/import.py --full-import --overwrite --read-defaults
-REM Copying TLG translation files...
-%snek% src/manage.py --move
-ECHO Imports complete!
-ECHO Removing furigana...
-%snek% src/ruby-remover.py
-ECHO Cleaning up outdated backups...
-%snek% src/filecopy.py --remove-old
-ECHO All done!
-GOTO quit
+IF [%1] NEQ [] ( 
+    GOTO %1 
+) ELSE (
+    ECHO Patching options:
+    ECHO 0. Everything
+    ECHO 1. UI ^(menus, etc^)
+    ECHO 2. MDB ^(skills, misisons, etc^)
+    ECHO 3. Dialogue ^(story^)
+    SET /P mode=Enter the numbers for the parts you wish to patch, in order ^(eg. 12, not 21^): 
+    CALL update.bat
+    IF !mode! EQU 123 (
+        SET mode=0
+    )
+    IF !mode! EQU 1 (
+        CALL :tlg
+    ) ELSE IF !mode! EQU 2 (
+        CALL :mdb
+    ) ELSE IF !mode! EQU 3 (
+        CALL :dialogue
+    ) ELSE IF !mode! EQU 12 (
+        CALL :tlg
+        CALL :mdb
+    ) ELSE IF !mode! EQU 13 (
+        CALL :tlg
+        CALL :dialogue
+    ) ELSE IF !mode! EQU 23 (
+        CALL :mdb
+        CALL :dialogue
+    ) ELSE IF !mode! EQU 0 (
+        CALL :tlg
+        CALL :mdb
+        CALL :dialogue
+    ) ELSE (
+        ECHO Invalid choice
+    )
+    GOTO quit
+)
 
 :install
 IF NOT EXIST ".venv" (
@@ -57,22 +76,33 @@ IF %ERRORLEVEL% NEQ 0 (
 %snek% src/post-install.py
 GOTO quit
 
+:uninstall
+ECHO Uninstalling patch...
+%snek% src/restore.py --uninstall
+GOTO quit
+
+:dialogue
+ECHO Importing all translatable types that are present in your game files...
+ECHO By default, already patched files are skipped. To forcefully rewrite all files, set update to false in the config.
+REM Or manually import parts, see import.py -h
+%snek% src/import.py --full-import --overwrite --read-defaults
+ECHO Removing furigana...
+%snek% src/ruby-remover.py
+ECHO Cleaning up outdated backups...
+%snek% src/filecopy.py --remove-old
+EXIT /B 0
+
 :mdb
-CALL update.bat
 ECHO Attempting mdb backup...
 ECHO Original will be stored at \Users\^<name^>\AppData\LocalLow\Cygames\umamusume\master\master.mdb.bak
 %snek% src/mdb/import.py --backup
 ECHO Importing mdb text...
 %snek% src/mdb/import.py --read-defaults
-GOTO quit
+EXIT /B 0
 
-:uninstall
-ECHO Uninstalling patch...
-%snek% src/restore.py --uninstall
-
-:quit
-PAUSE
-EXIT /B
+:tlg
+%snek% src/manage.py --move
+EXIT /B 0
 
 :venv
 IF EXIST ".venv" (
@@ -80,3 +110,7 @@ IF EXIST ".venv" (
     .venv\Scripts\activate.bat
 )
 EXIT /B 0
+
+:quit
+PAUSE
+EXIT /B

noccu/umamusu-translate

commit 293530d6cdf3328e7b798a1254fb599afac07482

Use pathlib everywhere
Was already mostly so but should now be complete.

src/filecopy.py
@@ -81,18 +81,17 @@ def removeOldFiles(args):
     if not args.overwrite and lastrun + 15778476 > ts:
         print(f"Skipping backup pruning as it was last done on {utils.timestampToDate(lastrun)}.")
         return 0, 0
-    from pathlib import Path, PurePath
 
     n = 0
     isHash = args.remove_old == "hash"
-    files = [Path(p) for p in patch.searchFiles(PurePath(args.dst), None, None, jsonOnly=False)]
+    files = patch.searchFiles(args.dst, None, None, jsonOnly=False)
     print(f"Found {len(files)} files in {args.dst}")
     with sqlite3.connect(GAME_META_FILE) as db:
-        for fPath in files:
-            q = f"h = '{fPath.name}'" if isHash else f"n like '%{fPath.name}'"
+        for file in files:
+            q = f"h = '{file.name}'" if isHash else f"n like '%{file.name}'"
             if not db.execute(f"select h from a where {q}").fetchone():
-                Path(fPath).unlink()
-                logger.debug(f"Removed {fPath}")
+                file.unlink()
+                logger.debug(f"Removed {file}")
                 n += 1
     cfg.core["lastBackupPrune"] = ts
     cfg.save()

src/common/constants.py
@@ -1,16 +1,17 @@
-from os import environ, name as osname, path
+from os import environ, name as osname
 from pathlib import Path
 
 IS_WIN = osname == "nt"
-DMM_CONFIG = Path(environ["APPDATA"]) / "dmmgameplayer5" / "dmmgame.cnf" if IS_WIN else None
+DMM_CONFIG = Path(environ["APPDATA"], "dmmgameplayer5/dmmgame.cnf") if IS_WIN else None
 
 if IS_WIN:
-    GAME_ROOT = path.realpath(path.join(environ["LOCALAPPDATA"], "../LocalLow/Cygames/umamusume/"))
-    GAME_ASSET_ROOT = path.join(GAME_ROOT, "dat")
-    GAME_META_FILE = path.join(GAME_ROOT, "meta")
-    GAME_MASTER_FILE = path.join(GAME_ROOT, "master", "master.mdb")
+    GAME_ROOT = Path(environ["LOCALAPPDATA"], "../LocalLow/Cygames/umamusume").resolve()
+    GAME_ASSET_ROOT = GAME_ROOT.joinpath("dat")
+    GAME_META_FILE = GAME_ROOT.joinpath("meta")
+    GAME_MASTER_FILE = GAME_ROOT.joinpath("master/master.mdb")
 else:
-    GAME_ROOT = GAME_ASSET_ROOT = GAME_META_FILE = GAME_MASTER_FILE = None
+    _none:Path = None  # Fuckery to gaslight IDE into common scenario
+    GAME_ROOT = GAME_ASSET_ROOT = GAME_META_FILE = GAME_MASTER_FILE = _none
 SUPPORTED_TYPES = [
     "story",
     "home",
@@ -22,3 +23,5 @@ SUPPORTED_TYPES = [
 ]  # Update indexing on next line
 TARGET_TYPES = SUPPORTED_TYPES[:-2]  # Classic asset types we want to read/write.
 NAMES_BLACKLIST = ["<username>", "", "モノローグ"]  # Special-use game names, don't touch
+
+TRANSLATION_FOLDER = Path("translations")

src/common/patch.py
@@ -4,29 +4,30 @@ import os
 import sys
 from datetime import datetime, timezone
 from pathlib import Path, PurePath
+from typing import Union
 
 import regex
 
 from . import utils, logger
-from .constants import GAME_ASSET_ROOT, TARGET_TYPES
+from .constants import GAME_ASSET_ROOT, TARGET_TYPES, TRANSLATION_FOLDER
 from .types import StoryId
 
 __IS_USING_TLG = None
 
 
 def searchFiles(
-    targetType,
+    targetType: Union[str, PurePath],
     targetGroup,
     targetId,
     targetIdx=None,
     targetSet=None,
     changed=False,
     jsonOnly=True,
-) -> list[str]:
+) -> list[Path]:
     def isJson(f: str):
-        return PurePath(f).suffix == ".json" if jsonOnly else True
+        return f.endswith(".json") if jsonOnly else True
 
-    found = list()
+    found: list[Path] = list()
     if changed:
         from subprocess import PIPE, run
 
@@ -50,20 +51,21 @@ def searchFiles(
                 and path.parts[0] == "translations"
                 and path.parts[1] == targetType
             ):
-                if not isJson(path):
+                if not isJson(path.name):
                     continue
                 if targetGroup and path.parts[2] != targetGroup:
                     continue
                 if targetId and path.parts[3] != targetId:
                     continue
-                found.append(str(path))
+                found.append(path)
     else:
         searchDir = (
             targetType
-            if isinstance(targetType, os.PathLike)
-            else os.path.join("translations", targetType)
+            if isinstance(targetType, PurePath)
+            else TRANSLATION_FOLDER.joinpath(targetType)
         )
         for root, dirs, files in os.walk(searchDir):
+            root = Path(root)
             dirType = len(dirs[0]) if dirs else -1
             if targetSet and dirType == 5 and targetSet in dirs:
                 dirs[:] = (targetSet,)
@@ -72,7 +74,7 @@ def searchFiles(
             elif targetId:
                 if targetType in ("lyrics", "preview"):
                     found.extend(
-                        os.path.join(root, file)
+                        root.joinpath(file)
                         for file in files
                         if PurePath(file).stem == targetId and isJson(file)
                     )
@@ -81,12 +83,12 @@ def searchFiles(
                     dirs[:] = (targetId,)
             if targetIdx and files:
                 found.extend(
-                    os.path.join(root, file)
+                    root.joinpath(file)
                     for file in files
                     if file.startswith(targetIdx) and isJson(file)
-                )
+                )  #todo: consider full filename check for flexibility
             else:
-                found.extend(os.path.join(root, file) for file in files if isJson(file))
+                found.extend(root.joinpath(file) for file in files if isJson(file))
     return found
 
 
@@ -150,8 +152,8 @@ class Args(argparse.ArgumentParser):
                 const=True,
                 help="Limit to changed files (requires git)",
             )
-            self.add_argument("-src", default=GAME_ASSET_ROOT)
-            self.add_argument("-dst", default=Path("dat/").resolve())
+            self.add_argument("-src", type=Path, default=GAME_ASSET_ROOT)
+            self.add_argument("-dst", type=Path, default=Path("dat/").resolve())
         elif types:
             self.add_argument(
                 "-t",

src/common/types.py
@@ -2,7 +2,7 @@ import json
 import os
 from dataclasses import asdict, astuple, dataclass
 from pathlib import Path, PurePath
-from typing import TYPE_CHECKING, Generator, Union, Optional
+from typing import TYPE_CHECKING, Generator, Optional
 
 import regex
 import UnityPy
@@ -110,7 +110,7 @@ class TranslationFile:
     ver_offset_mdb = 100
     textBlacklist = regex.compile(r"^タイトルコール$|イベントタイトルロゴ表示.*|※*ダミーテキスト|^欠番$")
 
-    def __init__(self, file: Union[str, Path] = None, load=True, readOnly=False):
+    def __init__(self, file: Path = None, load=True, readOnly=False):
         self.readOnly = readOnly
         if load:
             if not file:
@@ -299,8 +299,8 @@ class TranslationFile:
         else:
             self._snapshot = json.dumps(self.data, ensure_ascii=False, default=utils._to_json)
 
-    def setFile(self, file: Union[str, Path]):
-        self.file = Path(file)
+    def setFile(self, file: Path):
+        self.file = file
         self.name = self.file.name
 
     def init(self, snapshot=True):
@@ -412,7 +412,7 @@ class GameBundle:
 
         b = self.data.file.save() + self.patchData
         fn = dstName or self.data.file.name
-        fp = ((dstFolder / fn[0:2]) if dstFolder else self.bundlePath.parent) / fn
+        fp = dstFolder.joinpath(fn[0:2] if dstFolder else self.bundlePath.parent, fn)
         fp.parent.mkdir(parents=True, exist_ok=True)
         with open(fp, "wb") as f:
             f.write(b)

src/common/utils.py
@@ -1,14 +1,11 @@
 import json
 from datetime import datetime, timezone
-from pathlib import Path
-from typing import Union, TYPE_CHECKING, Optional
+from pathlib import Path, PurePath
+from typing import Union, Optional
 
 import regex
 
 from .constants import DMM_CONFIG, IS_WIN
-
-if TYPE_CHECKING:
-    from os import PathLike
 
 __GAME_INSTALL_DIR = None
 
@@ -87,13 +84,14 @@ def _to_json(o):
         raise TypeError
 
 
-def readJson(file: "PathLike") -> Union[dict, list]:
+def readJson(file: Union[str, PurePath]) -> Union[dict, list]:
     with open(file, "r", encoding="utf8") as f:
         return json.load(f)
 
 
-def writeJson(file: "PathLike", data, indent=4):
-    file = Path(file)
+def writeJson(file: Union[str, Path], data, indent=4):
+    if not isinstance(file, Path):
+        file = Path(file)
     file.parent.mkdir(parents=True, exist_ok=True)
     with open(file, "w", encoding="utf8", newline="\n") as f:
         json.dump(data, f, ensure_ascii=False, indent=indent, default=_to_json)

src/edit_story.py
@@ -1,11 +1,12 @@
 from argparse import SUPPRESS
+from pathlib import Path
 from common import constants, patch
 from editor.app import Editor
 
 
 def parseArgs(args=None):
     ap = patch.Args("Story editor", types=constants.SUPPORTED_TYPES)
-    ap.add_argument("-src")
+    ap.add_argument("-src", type=Path)
     ap.add_argument("-dst", help=SUPPRESS)
     args = ap.parse_args(args)
     return args

src/editor/files.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from tkinter import messagebox
 from typing import TYPE_CHECKING, Union
 
@@ -39,7 +40,7 @@ class FileManager:
         self.saveState = SaveState()
         self.formattedFiles = None
 
-    def importFiles(self, src: Union[StoryId, str, list[Union[TranslationFile, str]]]):
+    def importFiles(self, src: Union[StoryId, Path, list[Union[TranslationFile, Path]]]):
         if isinstance(src, StoryId):
             # todo: make search accept storyid
             files = patch.searchFiles(
@@ -47,10 +48,10 @@ class FileManager:
             )
             if not files:
                 raise FileNotFoundError("No files match given criteria")
-        elif isinstance(src, str):
+        elif isinstance(src, Path):
             files = [src]
         elif isinstance(src, list):
-            if not isinstance(src[0], (str, TranslationFile)):
+            if not isinstance(src[0], (Path, TranslationFile)):
                 raise ValueError
             files = src
         else:
@@ -58,16 +59,17 @@ class FileManager:
         files.sort()
         # todo: Was chapter_dropdown.formattedList
         self.files = files
-        self.formattedFiles = [f.split("\\")[-1] for f in files]
+        # f.name happens to work on both types
+        self.formattedFiles = [f.name for f in files]
         #! sigh
         self.master.nav.chapterPicker["values"] = self.formattedFiles
         self.master.nav.chapterPicker.current(0)
         self.master.nav.change_chapter()
-        # reset savestate and others?
+        #? reset savestate and others?
 
     def loadFile(self, chapter: int) -> TranslationFile:
         file = self.files[chapter]
-        if isinstance(file, str):
+        if isinstance(file, Path):
             file = TranslationFile(file)
             self.files[chapter] = file
         return file
@@ -77,7 +79,7 @@ class FileManager:
         targets = self.files if saveAll else (nav.cur_file,)
 
         for ch, file in enumerate(targets):
-            if saveAll and isinstance(file, str):
+            if saveAll and not isinstance(file, TranslationFile):
                 continue
             if self.master.options.markHuman.get():
                 file.data["humanTl"] = True

src/extract.py
@@ -267,7 +267,7 @@ class DataTransfer:
                     group["origLen"] = targetBlock["animData"][i]["origLen"]
 
 
-def exportAsset(bundle: Optional[str], path: str, db=None):
+def exportAsset(bundle: Optional[str], path: Union[str, PurePath], db=None):
     '''Exports an AssetBundle.
        :param path: internal Unity path
        :return: number of files changed'''
@@ -326,7 +326,7 @@ def exportAsset(bundle: Optional[str], path: str, db=None):
     # Remove invalid path chars (win)
     title = sanitizeFilename(outFile.data.get("title", ""))
     idxString = f"{storyId.idx} ({title})" if title else storyId.getFilenameIdx()
-    outFile.setFile(str(exportDir / f"{idxString}.json"))
+    outFile.setFile(exportDir.joinpath(f"{idxString}.json"))
     outFile.save()
     return 1
 

commit a8ccdb789e9bfefcaa20fbab711941300049a0b3

Fix cleanup of old backups not finding files
Regression stemming from 293530d6.

src/filecopy.py
@@ -1,6 +1,7 @@
 import shutil
 import sqlite3
 from os import makedirs, path
+from pathlib import PurePath
 
 from common import patch, utils, logger
 from common.constants import GAME_META_FILE, TARGET_TYPES
@@ -164,7 +165,7 @@ def parseArgs(args=None):
         action="store_true",
         help="Use full unity path in save dest, creating folders. Needs -name",
     )
-    ap.add_argument("-dst", default="dump/")
+    ap.add_argument("-dst", type=PurePath, default="dump/")
     ap.add_argument("-O", dest="overwrite", action="store_true", help="Overwrite existing")
     ap.add_argument(
         "-B",
6e4f7cc37e8c8300140853043c0abe3fe1cdab65
noccu <37862870+noccu@users.noreply.github.com>
2024-06-26 18:09:32 +0200

    Support old git name in installer

--- src/scripts/install-umatl.bat
+++ src/scripts/install-umatl.bat
@@ -9,16 +9,13 @@ IF %parent% EQU scripts (
 
 REM Check for existing install
 IF EXIST "UmaTL" (
-    SET /P reinstall=An UmaTL folder already exists. Replace it? [y]es, [n]o, [u]pdate: 
-    IF /I !reinstall! EQU y (
-        RMDIR /S /Q UmaTL
-        CALL :install
-    ) ELSE IF /I !reinstall! EQU u (
-        CALL patch.bat install
-        EXIT /B
-    ) ELSE ( 
-        ECHO No actions required, exiting.
-    )
+    SET name=UmaTL
+)
+IF EXIST "umamusu-translate" (
+    SET name=umamusu-translate
+)
+IF DEFINED name (
+    CALL :update
 ) ELSE (
     CALL :install
 )
@@ -34,6 +31,19 @@ MOVE uma-temp-mingit UmaTL\.mingit
 CD UmaTL
 CALL patch.bat install
 EXIT /B 0
+
+:update
+SET /P reinstall=Existing UmaTL found. What do you want to do? [u]pdate, [r]einstall, [e]xit:
+IF /I !reinstall! EQU r (
+    RMDIR /S /Q %name%
+    CALL :install
+) ELSE IF /I !reinstall! EQU u (
+    CALL patch.bat install
+    EXIT /B
+) ELSE (
+    ECHO No actions required, exiting.
+)
+EXIT /B
 
 :mingit
 ECHO Installing MinGit ^(https://github.com/git-for-windows/git/releases^)...
